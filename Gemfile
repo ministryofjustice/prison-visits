da0717b3841fbbae409d995443e9cd2c93b00c7c
Jan Szumiec <jan.szumiec@gmail.com>
2013-09-09 13:23:26 +0100

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+gem 'unicorn'
+gem 'byebug', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PrisonVisits2::Application.config.secret_key_base = 'fc626e30732938f9b856266f586b3baf6ae9415c868c4f25c8f76dc86ac192d8e6f654cf5ec7345d5c463fdb7ecf12b9ba4fce888b7dd6006e7e5c8bf7c3d108'
