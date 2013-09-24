c792a38fb950164d34b6c7f1463a6d68351cbfb1
Mat Harden <matharden@gmail.com>
2013-09-24 11:38:39 +0100

    Remove offline images hack

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,6 @@
 moj.init();
 
 // Hacks to show GOV.UK images when offline
-$('#logo img').attr('src', '/assets/gov.uk_logotype-2x.png');
-$('#footer .footer-meta .copyright a').css('backgroundImage', 'url(/assets/govuk-crest-2x.png)');
-$('.footer-meta .footer-meta-inner .open-government-licence h2 img').attr('src', '/assets/open-government-licence_2x.png');
+// $('#logo img').attr('src', '/assets/gov.uk_logotype-2x.png');
+// $('#footer .footer-meta .copyright a').css('backgroundImage', 'url(/assets/govuk-crest-2x.png)');
+// $('.footer-meta .footer-meta-inner .open-government-licence h2 img').attr('src', '/assets/open-government-licence_2x.png');
