ca8f3b8c3781f371337a9d3d46d994539703f9a5
Jan Szumiec <jan.szumiec@gmail.com>
2013-09-26 14:26:33 +0100

    Added a BookingRequest mailer.

---
+++ app/mailers/booking_request.rb
@@ -0,0 +1,6 @@
+class BookingRequest < ActionMailer::Base
+  def request_email(visit)
+    from = visit.visitors.find { |v| v.email }.email
+    mail(from: from, to: 'pvb-email-test@googlegroups.com', subject: 'Visit request')
+  end
+end
