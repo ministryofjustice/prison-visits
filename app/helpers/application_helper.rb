c3138f3d56a03e65a8367b067b28d348c748591b
Mat Harden <matharden@gmail.com>
2013-10-02 18:46:24 +0100

    Format dates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def display_time_slot(times, glue=' to ')
+    from, to = times.split('-')
+    [format_time_str(from), format_time_str(to)].join(glue)
+  end
+
+  def format_time_str(time)
+    Time.strptime(time, '%H%M').strftime("%l:%M%P")
+  end
 end
