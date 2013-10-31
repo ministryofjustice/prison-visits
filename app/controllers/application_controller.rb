96d879a097aa96e2ac236c3c9283a0063a328c3e
Mat Harden <matharden@gmail.com>
2013-10-31 22:23:13 +0000

    Removed authentication until password config is correct

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :visit
-  before_filter :authenticate
+  # before_filter :authenticate
   
   def authenticate
     if Rails.env.production?
