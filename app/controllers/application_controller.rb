2e583f79fc39fe1bcc1b4dc329de2276d83c3924
Mat Harden <matharden@gmail.com>
2013-10-31 17:04:26 +0000

    HTTP auth for production (staging)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :visit
+  http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD'] unless Rails.env.development?
 
   def visit
     session[:visit] ||= Visit.new(prisoner: Prisoner.new, visitors: [Visitor.new], slots: [])
