8d657cae2ff6a58f02a8835b1bbacb843ca31f95
Mat Harden <matharden@gmail.com>
2013-10-31 18:06:48 +0000

    Actually authenticate like a grown-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :visit
-  http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD'] unless Rails.env.development?
+  before_filter :authenticate
+  
+  def authenticate
+    unless Rails.env.development?
+      authenticate_with_http_basic do |username, password|
+          username == ENV['HTTP_USER'] && password == ENV['HTTP_PASSWORD']
+      end
+    end
+  end
 
   def visit
     session[:visit] ||= Visit.new(prisoner: Prisoner.new, visitors: [Visitor.new], slots: [])
