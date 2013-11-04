c24de60f28e6984af57bb0d60ae9ea2bf863eea9
Mat Harden <matharden@gmail.com>
2013-11-04 11:07:57 +0000

    Basic HTTP authentication - at last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :visit
-  # before_filter :authenticate
-  
-  def authenticate
-    if Rails.env.production?
-      request_http_basic_authentication do |username, password|
-        username == ENV['HTTP_USER'] && password == ENV['HTTP_PASSWORD']
-      end
-    end
-  end
+  http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD'] if Rails.env.production?
 
   def visit
     session[:visit] ||= Visit.new(prisoner: Prisoner.new, visitors: [Visitor.new], slots: [])
