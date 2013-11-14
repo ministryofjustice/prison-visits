e1a71ca4cf8820b72919a5c4b7ac9e851fe8db34
Jan Szumiec <jan.szumiec@gmail.com>
2013-11-14 16:19:58 +0000

    The session cookie expires after 15 minutes.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-PrisonVisits2::Application.config.session_store :cookie_store, key: 'pvbs'
+PrisonVisits2::Application.config.session_store :cookie_store, key: 'pvbs', expire_after: 15.minutes, httponly: true
---
+++ spec/configuration/session_cookie_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe PrisonVisits2::Application.config do
+  it "sets a cookie with the right parameters" do
+    subject.session_store.should == ActionDispatch::Session::CookieStore
+    subject.session_options.should == {key: "pvbs", expire_after: 15.minutes, httponly: true, cookie_only: true}
+  end
+end
