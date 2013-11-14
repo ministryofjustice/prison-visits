0d9243127a84dd7326a67a3cd17a41636da61219
Jan Szumiec <jan.szumiec@gmail.com>
2013-11-14 17:01:07 +0000

    PVBE-26 Sessions time out after 5 minutes.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-PrisonVisits2::Application.config.session_store :cookie_store, key: 'pvbs', expire_after: 15.minutes, httponly: true
+PrisonVisits2::Application.config.session_store :cookie_store, key: 'pvbs', expire_after: 5.minutes, httponly: true
--- spec/configuration/session_cookie_spec.rb
+++ spec/configuration/session_cookie_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe PrisonVisits2::Application.config do
   it "sets a cookie with the right parameters" do
     subject.session_store.should == ActionDispatch::Session::CookieStore
-    subject.session_options.should == {key: "pvbs", expire_after: 15.minutes, httponly: true, cookie_only: true}
+    subject.session_options.should == {key: "pvbs", expire_after: 5.minutes, httponly: true, cookie_only: true}
   end
 end
