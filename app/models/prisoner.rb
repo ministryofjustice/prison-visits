08e068072e54df8bc2c8e64c6ea8a66c2cd18a72
Mat Harden <matharden@gmail.com>
2013-11-18 18:19:42 +0000

    PVBE-39 Error message copy improvements

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -23,7 +23,7 @@ class Prisoner
 
   validates_presence_of :first_name
   validates_presence_of :last_name
-  validates_inclusion_of :date_of_birth, in: ->(_) { 100.years.ago.to_date..Date.today }, message: "must be within last 100 years"
+  validates_inclusion_of :date_of_birth, in: ->(_) { 100.years.ago.to_date..Date.today }, message: "must be a valid date of birth"
   validates_format_of :number, with: /\A[a-z]\d{4}[a-z]{2}\z/i, message: "must be a valid prisoner number" # eg a1234aa
   validates_inclusion_of :prison_name, in: PRISONS
 end
