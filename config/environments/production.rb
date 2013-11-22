fc3a4abbce30a8bf67707156a5bcef45c2b51789
Jan Szumiec <jan.szumiec@gmail.com>
2013-11-22 16:08:56 +0000

    Minor SMTP config update.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ PrisonVisits2::Application.configure do
   
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_HOSTNAME'],
-    port: 587,
+    port: ENV['SMTP_PORT'],
     domain: ENV['SMTP_DOMAIN'],
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
