f780276f2dafaaf1a1748d8bbf1fb85627f356e8
Jan Szumiec <jan.szumiec@gmail.com>
2013-11-26 11:42:57 +0000

    PVBE-102 Each supported prison has their own e-mail address.          Reduced the supported prisons to Rochester & Durham.

--- app/mailers/booking_request.rb
+++ app/mailers/booking_request.rb
@@ -1,8 +1,25 @@
 class BookingRequest < ActionMailer::Base
   add_template_helper(ApplicationHelper)
+
+  EMAILS = {
+    'Rochester' => 'socialvisits.rochester@hmps.gsi.gov.uk',
+    'Durham' => 'socialvisits.durham@hmps.gsi.gov.uk'
+  }
+
   def request_email(visit)
     @visit = visit
     user = visit.visitors.find { |v| v.email }.email
-    mail(from: 'prisonvisitsbooking@digital.justice.gov.uk', to: 'pvb-email-test@googlegroups.com', subject: 'Visit request', reply_to: user)
+
+    recipient = if production?
+      EMAILS[visit.prisoner.prison_name]
+    else
+      'pvb-email-test@googlegroups.com'
+    end
+
+    mail(from: 'prisonvisitsbooking@digital.justice.gov.uk', to: recipient, subject: 'Visit request', reply_to: user)
+  end
+
+  def production?
+    ENV['APP_PLATFORM'] == 'production'
   end
 end
