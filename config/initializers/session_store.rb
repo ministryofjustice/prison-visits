743bb3190e314ab57c19c4e925fb5a6b3698d10b
Mat Harden <matharden@gmail.com>
2013-12-03 16:50:22 +0000

    PVBE-26 reinstate session timeout as 20 minutes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-PrisonVisits2::Application.config.session_store :cookie_store, key: 'pvbs', httponly: true
+PrisonVisits2::Application.config.session_store :cookie_store, key: 'pvbs', expire_after: 20.minutes, httponly: true
--- spec/configuration/session_cookie_spec.rb
+++ spec/configuration/session_cookie_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe PrisonVisits2::Application.config do
   it "sets a cookie with the right parameters" do
     subject.session_store.should == ActionDispatch::Session::CookieStore
-    subject.session_options.should == {key: "pvbs", httponly: true, cookie_only: true}
+    subject.session_options.should == {key: "pvbs", expire_after: 20.minutes, httponly: true, cookie_only: true}
   end
 end
