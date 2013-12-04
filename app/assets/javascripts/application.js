ff7cbdcf8266483ebc4eaf5f96b35055d66a8464
Mat Harden <matharden@gmail.com>
2013-12-04 22:07:26 +0000

    Small tidy of JavaScript files
    * Put Array forEachWithIndex into own lib file
    * Remove redundant JS
    * Move vendor code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,14 @@
 //= require Date.format
 //= require Array.move
+//= require Array.foreachwithindex
 
 //= require lodash
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
 //= require jquery.details
 
-//= require jquery-ui
-
 //= require moj
-//= require modules/moj.tabs
 //= require modules/moj.cookie-message
 //= require modules/moj.hijacks
 //= require modules/moj.effects
@@ -27,9 +26,3 @@ $(function () {
 // $('#logo img').attr('src', '/assets/gov.uk_logotype-2x.png');
 // $('#footer .footer-meta .copyright a').css('backgroundImage', 'url(/assets/govuk-crest-2x.png)');
 // $('.footer-meta .footer-meta-inner .open-government-licence h2 img').attr('src', '/assets/open-government-licence_2x.png');
-
-Array.prototype.forEachWithIndex = function(f) {
-    for (var i = 0; i < this.length; i++) {
-        f(this[i], i);
-    }
-}
