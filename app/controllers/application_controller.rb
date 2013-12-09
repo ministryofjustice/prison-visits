7f17dc75deeb5aaf6cb20c08cc4f2ae94c63bdc8
Jan Szumiec <jan.szumiec@gmail.com>
2013-12-09 14:13:18 +0000

    Removed basic auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :visit
-  http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD'] if Rails.env.production?
 
   def visit
     session[:visit] ||= Visit.new(prisoner: Prisoner.new, visitors: [Visitor.new], slots: [])
