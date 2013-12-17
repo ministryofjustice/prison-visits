ff6bad3579edf844df380d0d368be1862bc29b6e
Jan Szumiec <jan.szumiec@gmail.com>
2013-12-17 16:57:19 +0000

    Each support ticket should have the prison_visits tag set on it.

--- lib/zendesk_helper.rb
+++ lib/zendesk_helper.rb
@@ -1,6 +1,6 @@
 module ZendeskHelper
   def self.send_to_zendesk(feedback, client=ZENDESK_CLIENT)
-    ZendeskAPI::Ticket.new(client, description: feedback.text, requester: { email: feedback.email }, custom_fields: [{id: '23730083', value: feedback.referrer}]).save
+    ZendeskAPI::Ticket.new(client, description: feedback.text, requester: { email: feedback.email }, custom_fields: [{id: '23730083', value: feedback.referrer}, {id: '23757677', value: 'prison_visits'}]).save
   end
 end
 
