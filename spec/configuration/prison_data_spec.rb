de8a10c0e6de651ec6026e35addc9ceb2862d48b
Mat Harden <matharden@gmail.com>
2014-01-15 17:21:20 +0000

    Test for valid unbookbale dates
    This means we need a test to make sure these dates are actually unbookable.

--- spec/configuration/prison_data_spec.rb
+++ spec/configuration/prison_data_spec.rb
@@ -9,24 +9,26 @@ describe "Prison data" do
     subject.should have_key('Rochester')
     subject['Rochester']['enabled'].should be_true
     Rails.configuration.prison_data.should have_key('Rochester')
-
-    subject['Rochester']['unbookable'].should == ['2013-12-25', '2013-12-26', '2014-12-25', '2014-12-26'].map { |d| Date.parse(d) }
   end
 
   it "should contain an entry for Durham" do
     subject.should have_key('Durham')
     subject['Durham']['enabled'].should be_false
     Rails.configuration.prison_data.should_not have_key('Durham')
-
-    subject['Durham']['unbookable'].should == ['2014-04-18', '2014-04-20', '2014-12-25', '2014-12-26', '2015-01-01'].map { |d| Date.parse(d) }
   end
 
   it "should contain an entry for Cardiff" do
     subject.should have_key('Cardiff')
     subject['Cardiff']['enabled'].should be_false
     Rails.configuration.prison_data.should_not have_key('Cardiff')
+  end
 
-    subject['Cardiff']['unbookable'].should == ['2014-04-20', '2014-12-25', '2014-12-26', '2015-01-01'].map { |d| Date.parse(d) }
+  it "each unbookable date for each prison should be a valid date" do
+    subject.values.each do |prison|
+      prison['unbookable'].each do |d|
+        d.should be_a(Date)
+      end
+    end
   end
 
   ['phone', 'email', 'address', 'unbookable', 'slots', 'enabled'].each do |attribute|
