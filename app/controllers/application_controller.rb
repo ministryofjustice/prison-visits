91ae81472d8e63e6427fc2270f081cd8e066af82
Mat Harden <matharden@gmail.com>
2014-01-21 17:20:33 +0000

    Put password back on staging
    [Finishes #64237402]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :visit
 
+  if Rails.env.production? && ENV['APP_PLATFORM'] != 'production'
+    http_basic_authenticate_with name: ENV['HTTP_USER'], password: ENV['HTTP_PASSWORD']
+  end
+
   def visit
     session[:visit] ||= Visit.new(prisoner: Prisoner.new, visitors: [Visitor.new], slots: [])
   end
