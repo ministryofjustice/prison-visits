33c1bd2b780824cbcb8efffb6bd170496787ded5
Mat Harden <matharden@gmail.com>
2014-01-27 11:59:00 +0000

    Required cookie and JavaScript message
    [#63881424]

--- app/assets/javascripts/shame.js
+++ app/assets/javascripts/shame.js
@@ -8,3 +8,19 @@ function isElementInViewport (el) {
         rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
     );
 }
+
+moj.are_cookies_enabled = function () {
+  var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+
+  if (typeof navigator.cookieEnabled == "undefined" && !cookieEnabled) {
+    document.cookie="testcookie";
+    cookieEnabled = (document.cookie.indexOf("testcookie") != -1) ? true : false;
+  }
+  return (cookieEnabled);
+}
+
+moj.show_cookie_message = function () {
+  if (!moj.are_cookies_enabled()) {
+    $('noscript').after( $('noscript').text() );
+  }
+}()
