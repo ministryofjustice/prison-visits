25d3b3cfe251f1293a51476732f03c95a6db1677
Jan Szumiec <jan.szumiec@gmail.com>
2014-02-18 17:28:46 +0000

    Added a Secure: flag to the cookie.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
-PrisonVisits2::Application.config.session_store :cookie_store, key: 'pvbs', expire_after: 20.minutes, httponly: true
+{key: 'pvbs', expire_after: 20.minutes, httponly: true}.tap do |configuration|
+  configuration[:secure] = Rails.env.production?
+  PrisonVisits2::Application.config.session_store :cookie_store, configuration
+end
--- spec/configuration/session_cookie_spec.rb
+++ spec/configuration/session_cookie_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe PrisonVisits2::Application.config do
   it "sets a cookie with the right parameters" do
     subject.session_store.should == ActionDispatch::Session::CookieStore
-    subject.session_options.should == {key: "pvbs", expire_after: 20.minutes, httponly: true, cookie_only: true}
+    # secure: false -> true gets set by the initializer.
+    subject.session_options.should == {key: "pvbs", expire_after: 20.minutes, httponly: true, cookie_only: true, secure: false}
   end
 end
