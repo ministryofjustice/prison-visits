32544142f10e0be4fb583dc15d9b3c08993ee314
Jan Szumiec <jan.szumiec@gmail.com>
2014-06-06 12:40:26 +0100

    Enabled logging for elasticsearch operations.

--- config/initializers/metrics_logger.rb
+++ config/initializers/metrics_logger.rb
@@ -1,4 +1,7 @@
 require 'metrics_logger'
 
-METRICS_LOGGER = MetricsLogger.new(Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL'], log: Rails.logger))
+logger = Logger.new(File.join(Rails.root, 'log', 'elasticsearch.log'))
+
+METRICS_LOGGER = MetricsLogger.new(Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL'], log: true, logger: logger))
+
 
