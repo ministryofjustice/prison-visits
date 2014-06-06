d96a552ddc3014363ad38c26d94e603f2f733137
Mat Harden <matharden@gmail.com>
2014-06-06 15:33:33 +0100

    Merge feature/autocomplete
    Conflicts:
    	config/prison_data_production.yml
    	config/prison_data_staging.yml

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,12 +5,14 @@
 
 // Vendor assets
 //= require jquery.details
+//= require jquery-ui-autocomplete
 
 // Project assets
 //= require visitors
 
 // Candidates for re-usable components
 //= require date-picker
+//= require modules/moj.autocomplete
 //= require modules/moj.slot-picker
 //= require modules/moj.hijacks
 //= require modules/moj.date-slider
