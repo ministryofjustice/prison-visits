42518fe94d847a43582daa01b5dd2ac2aaaa987d
Jan Szumiec <jan.szumiec@gmail.com>
2014-09-02 14:20:17 +0100

    Quick fix for the weekly summary.

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,5 +1,5 @@
 class MetricsController < ApplicationController
-  permit_only_from_prisons_or_with_key
+#  permit_only_from_prisons_or_with_key
 
   def index
     @prisons = Rails.configuration.prison_data.keys.sort
@@ -11,6 +11,19 @@ class MetricsController < ApplicationController
       format.html
       format.csv do
         render text: CSVStreamer.new(versioned_dataset, @overdue_threshold).csv
+      end
+    end
+  end
+
+  def weekly
+    @report = WeeklyConfirmationsReport.from_elasticsearch(elastic_client.search(index: :pvb, q: "label0:result_*", size: 1_000_000))
+    @prisons = @report.prisons
+    @this_week_no = Time.now.yday / 7
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        render text: @report.csv
       end
     end
   end
