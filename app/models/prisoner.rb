e41ba6d99dcbb62b193bb47df72b519f23b54275
Mat Harden <matharden@gmail.com>
2014-09-04 08:03:46 +0100

    Add all prisons

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -14,6 +14,7 @@ class Prisoner
   validates_inclusion_of :date_of_birth, in: ->(_) { 100.years.ago.beginning_of_year..Time.now }, if: ->(p) { p.date_of_birth }, message: "must be a valid date of birth"
   validates_format_of :number, with: /\A[a-z]\d{4}[a-z]{2}\z/i, message: "must be a valid prisoner number" # eg a1234aa
   validates_inclusion_of :prison_name, in: Rails.configuration.prison_data.map{|k,v|k}.sort, message: "must be chosen"
+  validate :prison_in_service
 
   def full_name
     [@first_name, @last_name].join(' ')
@@ -26,6 +27,17 @@ class Prisoner
   def age
     if date_of_birth
       (Date.today - date_of_birth.to_date).to_i / 365
+    end
+  end
+  
+  def prison_data(source=visit)
+    Rails.configuration.prison_data[source.prisoner.prison_name.to_s]
+  end
+
+  def prison_in_service
+    unless Rails.configuration.prison_data[self.prison_name.to_s]['enabled']
+      errors.add(:prison_name, 'is not avaiable')
+      errors.add(:prison_name_reason, true)
     end
   end
 end
