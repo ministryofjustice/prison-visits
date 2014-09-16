749d0c78e25b503d2c621a1ed513617182ebfcb8
Mat Harden <matharden@gmail.com>
2014-09-16 14:30:47 +0100

    Group specs for enabled prisons

--- spec/configuration/prison_data_spec.rb
+++ spec/configuration/prison_data_spec.rb
@@ -36,47 +36,54 @@ describe "Prison data" do
       Rails.configuration.prison_data.should have_key('Cardiff')
     end
 
-    it "each unbookable date for each prison should be a valid date" do
-      subject.values.each do |prison|
-        prison['unbookable'].each do |d|
-          d.should be_a(Date)
-        end
-      end
-    end
-
-    it "each enabled prison has a valid e-mail address" do
-      subject.values.each do |prison|
-        Mail::Address.new(prison['email']).to_s.should_not be_empty if prison['enabled']
-      end
-    end
-
-    ['phone', 'email', 'address', 'unbookable', 'slots', 'enabled'].each do |attribute|
-      it "each section has a #{attribute} section" do
-        subject.values.each do |prison|
-          prison.should have_key(attribute)
-        end
-      end
-    end
-
-    it "should contain lowercase email addresses for all prisons" do
-      subject.values.each do |prison|
-        prison['email'].should match(/^[a-z0-9@\.-]+$/)
-      end
-    end
-
-    it "should contain slots which are 9 chars long for all prisons" do
-      subject.values.each do |prison|
-        prison['slots'].each do |day, times|
-          times.each do |time|
-            time.should match(/^[0-9-]{9}$/)
-          end
-        end
-      end
-    end
-
     it "should contain no duplicate prisons" do
       prisons_names = production_prison_data_as_text.reject{ |l| l.match(/^\s+/i) } # plain text as Yaml allows duplicates and uses latest
       prisons_names.detect{ |e| prisons_names.count(e) > 1 }.should be(nil)
+    end
+
+    context "enabled prisons" do
+      let :subject do
+        production_prison_data.reject!{|key, val|!key['enabled']}
+      end
+
+      it "each unbookable date for each prison should be a valid date" do
+        subject.values.each do |prison|
+          prison['unbookable'].each do |d|
+            d.should be_a(Date)
+          end
+        end
+      end
+
+      it "each enabled prison has a valid e-mail address" do
+        subject.values.each do |prison|
+          Mail::Address.new(prison['email']).to_s.should_not be_empty
+        end
+      end
+
+      ['phone', 'email', 'address', 'unbookable', 'slots', 'enabled'].each do |attribute|
+        it "each section has a #{attribute} section" do
+          subject.values.each do |prison|
+            prison.should have_key(attribute)
+          end
+        end
+      end
+
+      it "should contain lowercase email addresses for all prisons" do
+        subject.values.each do |prison|
+          prison['email'].should match(/^[a-z0-9@\.-]+$/)
+        end
+      end
+
+      it "should contain slots which are 9 chars long for all prisons" do
+        subject.values.each do |prison|
+          prison['slots'].each do |day, times|
+            times.each do |time|
+              time.should match(/^[0-9-]{9}$/)
+            end
+          end
+        end
+      end
+
     end
   end
 
