7d80060a339dbbbeb10d76d5fec0eedf9d2ffee8
Najaf Ali <ali.najaf@gmail.com>
2014-09-18 15:44:36 +0100

    Limits name length to 30 bytes

--- lib/name_validator.rb
+++ lib/name_validator.rb
@@ -1,6 +1,8 @@
 class NameValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if value && (value.include?('<') || value.include?('>'))
+    return unless value
+
+    if value.bytes.length > 30 || value.include?('<') || value.include?('>')
       record.errors.add(attribute, "is not a valid name")
     end
   end
