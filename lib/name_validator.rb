e01a8ce9cfe0ea5f54462fe7a46a0d99b3eec31d
Najaf Ali <ali.najaf@gmail.com>
2014-09-18 15:51:34 +0100

    Counts characters instead of bytes when limiting name length

--- lib/name_validator.rb
+++ lib/name_validator.rb
@@ -2,7 +2,7 @@ class NameValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return unless value
 
-    if value.bytes.length > 30 || value.include?('<') || value.include?('>')
+    if value.length > 30 || value.include?('<') || value.include?('>')
       record.errors.add(attribute, "is not a valid name")
     end
   end
