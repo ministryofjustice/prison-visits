af0ab42023702b17f46631d9453cb3efb5bc73a3
Jan Szumiec <jan.szumiec@gmail.com>
2014-11-11 17:03:42 +0000

    * Some tests.

--- app/controllers/instant/visitors_details_controller.rb
+++ app/controllers/instant/visitors_details_controller.rb
@@ -1,6 +1,13 @@
 class Instant::VisitorsDetailsController < ApplicationController
+  include CookieGuard
+  include SessionGuard
+
   def edit
     @collect_phone_number = false
     render 'shared/visitors_details/edit'
+  end
+
+  def update
+    render text: 'OK'
   end
 end
--- spec/controllers/instant/visitors_details_controller_spec.rb
+++ spec/controllers/instant/visitors_details_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Instant::VisitorsDetailsController do
+  render_views
 
+  before :each do
+    session[:visit] = Visit.new(visit_id: SecureRandom.hex, prisoner: Prisoner.new, visitors: [Visitor.new])
+    cookies['cookies-enabled'] = 1
+  end
+
+  it_behaves_like "a browser without a session present"
+  it_behaves_like "a session timed out"
 end
