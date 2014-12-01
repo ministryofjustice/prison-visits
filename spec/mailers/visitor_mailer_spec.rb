c16932a98636449fa95b0599db9ac656550e8b6a
Mat Harden <matharden@gmail.com>
2014-12-01 17:39:32 +0000

    Mailer spec for reference in canned responses

--- spec/mailers/visitor_mailer_spec.rb
+++ spec/mailers/visitor_mailer_spec.rb
@@ -43,6 +43,10 @@ describe VisitorMailer do
     Confirmation.new(message: 'A message', vo_number: '5551234', outcome: 'slot_0')
   end
 
+  let :confirmation_canned_response do
+    Confirmation.new(message: 'A message', vo_number: '5551234', outcome: 'slot_0', canned_response: true)
+  end
+
   let :confirmation_unlisted_visitors do
     Confirmation.new(vo_number: '5551234', outcome: 'slot_0', visitor_not_listed: true, unlisted_visitors: ['Joan;Harris'])
   end
@@ -117,6 +121,21 @@ describe VisitorMailer do
         email.body.raw_source.should include("phone: 01634 803100")
         email.body.raw_source.should_not include("Jimmy Harris")
         email.body.raw_source.should include('A message')
+      end
+
+      it "sends out an e-mail with a reference number (canned responses)" do
+        email = subject.booking_confirmation_email(sample_visit, confirmation_canned_response)
+        email.subject.should == "Visit confirmed: your visit for 7 July 2013 has been confirmed"
+
+        email[:from].should == noreply_address
+        email[:reply_to].should == prison_address
+        email[:to].should == visitor_address
+
+        email.body.raw_source.should include("email: pvb.rochester@maildrop.dsd.io")
+        email.body.raw_source.should include("phone: 01634 803100")
+        email.body.raw_source.should_not include("Jimmy Harris")
+        email.body.raw_source.should include('A message')
+        email.body.raw_source.should include('5551234')
       end
 
       it "sends out an e-mail with the list of visitors not on the approved visitor list" do
