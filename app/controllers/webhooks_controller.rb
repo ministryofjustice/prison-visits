b525db968cb721c5ab7cae8b8b1daf96fa49dc8d
Jan Szumiec <jan.szumiec@gmail.com>
2014-12-19 14:42:10 +0000

    I've put some metrics around the webhooks controller.

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -4,19 +4,23 @@ class WebhooksController < ApplicationController
       p = ParsedEmail.parse(email_params)
       
       unless p.to.local == 'no-reply'
+        STATSD_CLIENT.increment('pvb.app.email.unknown_recipient')
         render text: 'Unknown recipient'
         return
       end
       
       case p.source
       when :prison
+        STATSD_CLIENT.increment('pvb.app.email.autorespond_to_prison')
         PrisonMailer.autorespond(p).deliver
       when :visitor
+        STATSD_CLIENT.increment('pvb.app.email.autorespon_to_visitor')
         VisitorMailer.autorespond(p).deliver
       end
       
       render text: "Accepted."
     else
+      STATSD_CLIENT.increment('pvb.app.email.unauthorized')
       render text: "Unauthorized.", status: 403
     end
   rescue ParsedEmail::ParseError, ArgumentError
