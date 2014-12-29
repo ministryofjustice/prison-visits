1ae6bf5cf8dc0a9f4be73b3c2d785fbfdfe32374
Jan Szumiec <jan.szumiec@gmail.com>
2014-12-29 10:26:39 +0100

    Removed no longer used state restoration code.

--- config/initializers/encryptor.rb
+++ config/initializers/encryptor.rb
@@ -1,13 +1,13 @@
-require 'encryptor'
+require 'zmarshal'
 
 MESSAGE_ENCRYPTOR = \
 if key = ENV['MESSAGE_ENCRYPTOR_SECRET_KEY']
-  Encryptor.new(Base64.decode64(key))
+  ActiveSupport::MessageEncryptor.new(Base64.decode64(key), serializer: ZMarshal)
 else
   if Rails.env.production? && File.basename($0) != 'rake'
     raise "Missing MESSAGE_ENCRYPTOR_SECRET_KEY"
   else
-    Encryptor.new("TROLOLOLOLO" * 10)
+    ActiveSupport::MessageEncryptor.new("TROLOLOLOLO" * 10, serializer: ZMarshal)
   end
 end
 
