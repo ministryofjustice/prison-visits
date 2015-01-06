06df9abd37731fe7a96afab55a3c31a179283e59
Jan Szumiec <jan.szumiec@gmail.com>
2015-01-06 14:56:06 +0000

    Added another endpoint (bounced).

--- lib/sendgrid_helper.rb
+++ lib/sendgrid_helper.rb
@@ -1,15 +1,27 @@
 module SendgridHelper
   def self.spam_reported?(email)
-    body = Curl::Easy.perform(spam_reported_url(email)).body_str
+    handle_response(spam_reported_url(email))
+  end
+
+  def self.bounced?(email)
+    handle_response(bounced_url(email))
+  end
+
+  def self.spam_reported_url(email)
+    "https://sendgrid.com/api/spamreports.get.json?api_user=#{ENV['SMTP_USERNAME']}&api_key=#{ENV['SMTP_PASSWORD']}&email=#{email}"
+  end
+
+  def self.bounced_url(email)
+    "https://sendgrid.com/api/bounces.get.json?api_user=#{ENV['SMTP_USERNAME']}&api_key=#{ENV['SMTP_PASSWORD']}&email=#{email}"
+  end
+
+  def self.handle_response(url)
+    body = Curl::Easy.perform(url).body_str
     if response = JSON.parse(body)
       return false if response.is_a?(Hash) && response[:error]
       response.size > 0
     end
   rescue Curl::Err::CurlError, JSON::ParserError => e
     false
-  end
-
-  def self.spam_reported_url(email)
-    "https://sendgrid.com/api/spamreports.get.json?api_user=#{ENV['SMTP_USERNAME']}&api_key=#{ENV['SMTP_PASSWORD']}&email=#{email}"
   end
 end
--- spec/lib/sendgrid_helper_spec.rb
+++ spec/lib/sendgrid_helper_spec.rb
@@ -2,31 +2,63 @@ require 'spec_helper'
 require 'sendgrid_helper'
 
 describe SendgridHelper do
-  context "error handling" do
-    [Curl::Err::CurlError, JSON::ParserError].each do |exception_class|
-      it "marks the email as valid if there's an error (#{exception_class})" do
-        Curl::Easy.should_receive(:perform).and_raise(exception_class)
+  context "spam reports" do
+    context "error handling" do
+      [Curl::Err::CurlError, JSON::ParserError].each do |exception_class|
+        it "marks the email as valid if there's an error (#{exception_class})" do
+          Curl::Easy.should_receive(:perform).and_raise(exception_class)
+        end
+      end
+
+      it "marks the email as valid when authentication fails" do
+        JSON.stub(:parse).and_return({error: 'lol'})
+      end
+
+      after :each do
+        SendgridHelper.spam_reported?('test@irrelevant.com').should be_false
       end
     end
 
-    it "marks the email as valid when authentication fails" do
-      JSON.stub(:parse).and_return({error: 'lol'})
-    end
+    context "when no error" do
+      it "marks an e-mail as valid" do
+        JSON.should_receive(:parse).and_return(['dummy'])
+        SendgridHelper.spam_reported?('test@irrelevant.com')
+      end
 
-    after :each do
-      SendgridHelper.spam_reported?('test@irrelevant.com').should be_false
+      it "marks an e-mail as invalid" do
+        JSON.should_receive(:parse).and_return([])
+        SendgridHelper.spam_reported?('test@irrelevant.com')
+      end
     end
   end
 
-  context "when no error" do
-    it "marks an e-mail as valid" do
-      JSON.should_receive(:parse).and_return(['dummy'])
-      SendgridHelper.spam_reported?('test@irrelevant.com')
+  context "bounced" do
+    context "error handling" do
+      [Curl::Err::CurlError, JSON::ParserError].each do |exception_class|
+        it "marks the email as valid if there's an error (#{exception_class})" do
+          Curl::Easy.should_receive(:perform).and_raise(exception_class)
+        end
+      end
+
+      it "marks the email as valid when authentication fails" do
+        JSON.stub(:parse).and_return({error: 'lol'})
+      end
+
+      after :each do
+        SendgridHelper.bounced?('test@irrelevant.com').should be_false
+      end
     end
 
-    it "marks an e-mail as invalid" do
-      JSON.should_receive(:parse).and_return([])
-      SendgridHelper.spam_reported?('test@irrelevant.com')
+    context "when no error" do
+      it "marks an e-mail as valid" do
+        JSON.should_receive(:parse).and_return(['dummy'])
+        SendgridHelper.bounced?('test@irrelevant.com')
+      end
+
+      it "marks an e-mail as invalid" do
+        JSON.should_receive(:parse).and_return([])
+        SendgridHelper.bounced?('test@irrelevant.com')
+      end
     end
   end
 end
