b533e2860e96f8d85e3701ceaa59c64d2c7a0f6e
Mat Harden <matharden@gmail.com>
2015-01-06 00:11:33 +0000

    Improve readability of checkbox summary specs

--- spec/javascripts/checkbox-summary_Spec.js
+++ spec/javascripts/checkbox-summary_Spec.js
@@ -15,43 +15,43 @@ describe('CheckboxSummary', function() {
       expect(JSON.stringify(subject.settings)).toBe(JSON.stringify(subject.defaults));
     });
 
-    it('should be overridden by options when specified', function() {
+    it('should override defaults when options are specified', function() {
       var options = {glue: '-', strip: ':', sub: '_', original: 'original'},
           subject = new moj.Modules._CheckboxSummary($fixture, options);
       expect(JSON.stringify(subject.settings)).toBe(JSON.stringify(options));
     });
 
-    it('summary items are separated by commas by default', function() {
+    it('should separate summary items with commas by default', function() {
       $fixture.find('#Orange, #Green').prop('checked', true).change();
       expect($fixture.find('.CheckboxSummary-summary').text()).toBe('Orange, Green');
     });
 
-    it('correctly summarises checked selections between every change', function() {
+    it('should remove text from summaries when checked selections are un-checked', function() {
       $fixture.find('#Orange, #Green').prop('checked', true).change();
       expect($fixture.find('.CheckboxSummary-summary').text()).toBe('Orange, Green');
       $fixture.find('#Orange').prop('checked', false).change();
       expect($fixture.find('.CheckboxSummary-summary').text()).toBe('Green');
     });
 
-    it('summary items are separated by optional string', function() {
+    it('should separate items with a string when specified', function() {
       var subject = new moj.Modules._CheckboxSummary($fixture, {glue: ' - '});
       $fixture.find('#Orange, #Green').prop('checked', true).change();
       expect($fixture.find('.CheckboxSummary-summary').text()).toBe('Orange - Green');
     });
 
-    it('optional list of characters can be removed from summary text', function() {
+    it('should replace a list of characters from summary text', function() {
       var subject = new moj.Modules._CheckboxSummary($fixture, {strip: 'e', sub: 'o'});
       $fixture.find('#Green').prop('checked', true).change();
       expect($fixture.find('.CheckboxSummary-summary').text()).toBe('Groon');
     });
 
-    it('original placeholder text is used when summary is blank', function() {
+    it('should display placeholder text when no items are checked', function() {
       $fixture.find('#Orange').prop('checked', true).change();
       $fixture.find('#Orange').prop('checked', false).change();
       expect($fixture.find('.CheckboxSummary-summary').text()).toBe('[summary]');
     });
 
-    it('optional placeholder text is used when specified', function() {
+    it('should display custom placeholder text when specified', function() {
       $fixture = $('<div class="CheckboxSummary"><input type="checkbox" value="Blue" id="Blue" /><span class="CheckboxSummary-summary">something else</span></div>');
       $fixture.find('#Blue').prop('checked', true).change();
       $fixture.find('#Blue').prop('checked', false).change();
@@ -59,24 +59,25 @@ describe('CheckboxSummary', function() {
     });
   });
 
-  it('elements are cached', function() {
+  it('caches elements for reuse', function() {
     expect(subject.$checkboxes.length).toBe(2);
+    expect(subject.$summaries.length).toBe(1);
   });
 
-  it('render is triggered from moj.Events module', function() {
+  it('is rendered by moj.Events module', function() {
     $fixture = $('<div class="CheckboxSummary"><input type="checkbox" value="Orange" id="Orange" /><input type="checkbox" value="Green" id="Green" checked="checked" /><span class="CheckboxSummary-summary"></span></div>'),
     subject = new moj.Modules._CheckboxSummary($fixture);
     moj.Events.trigger('render');
     expect($fixture.find('.CheckboxSummary-summary').text()).toBe('Green');
   });
 
-  it('summaries are limited to currently checked boxes', function() {
+  it('creates a summary from currently checked boxes', function() {
     $fixture.find('#Orange').prop('checked', true).change();
     expect($fixture.find('.CheckboxSummary-summary').text()).toBe('Orange');
     expect(subject.getChecked(subject.$checkboxes).length).toBe(1);
   });
 
-  it('summary text is taken from checkbox values', function() {
+  it('uses checkbox values as summary text', function() {
     $fixture.find('#Orange').prop('checked', true).change();
     expect($fixture.find('.CheckboxSummary-summary').text()).toBe('Orange');
   });
