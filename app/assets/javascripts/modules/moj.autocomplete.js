d07d4cc3062897ef376b965bf58c7fc49b631015
Mat Harden <matharden@gmail.com>
2015-01-08 14:47:33 +0000

    Consistently use single quotes in JavaScript

--- app/assets/javascripts/modules/moj.autocomplete.js
+++ app/assets/javascripts/modules/moj.autocomplete.js
@@ -5,11 +5,11 @@
 
 (function (window, $) {
   
-  "use strict";
+  'use strict';
 
   var MojAutocomplete = function(el, options){
     // avoid applying twice
-    if(!el.data("moj.autocomplete")){ // TODO: find a better way...
+    if(!el.data('moj.autocomplete')){ // TODO: find a better way...
       // set settings
       this.settings = $.extend({}, this.defaults, options);
       // cache element
@@ -17,7 +17,7 @@
       // build new elements
       this._create();
 
-      el.data("moj.autocomplete", true);
+      el.data('moj.autocomplete', true);
     }
   };
 
@@ -34,7 +34,7 @@
 
     _create: function(){
       // create and hide el
-      this.$wrapper = $("<span>").addClass("moj-autocomplete").insertAfter(this.$select);
+      this.$wrapper = $('<span>').addClass('moj-autocomplete').insertAfter(this.$select);
       // build autocomplete field
       this._createAutocomplete();
       // hide original select el
@@ -42,13 +42,13 @@
     },
 
     _createAutocomplete: function(){
-      var selected = this.$select.children(":selected"),
-          val = selected.val() ? selected.text() : "",
+      var selected = this.$select.children(':selected'),
+          val = selected.val() ? selected.text() : '',
           key, value, i, attrs, raw_attrs;
 
-      this.$text = $( "<input>" ).attr("type", "text") // give it a field type
+      this.$text = $( '<input>' ).attr('type', 'text') // give it a field type
                                   .val(val) // set value if already selected
-                                  .data("select", this.$select); // assoc select with this input
+                                  .data('select', this.$select); // assoc select with this input
 
       // if required, copy across attributes - useful for using [placeholder]
       if (this.settings.copyAttr) {
@@ -58,25 +58,25 @@
         for (i=0; i < raw_attrs.length; i++) {
           key = raw_attrs[i].nodeName;
           value = raw_attrs[i].nodeValue;
-          if ( key !== "name" && key !== "id" && key !== "class" && typeof this.$select.attr(key) !== "undefined" ) {
+          if ( key !== 'name' && key !== 'id' && key !== 'class' && typeof this.$select.attr(key) !== 'undefined' ) {
             attrs[key] = value;
           }
         }
         this.$text.attr(attrs);
       }
 
-      this.settings.autocomplete.source = $.proxy(this, "_source"); // use source method from class
+      this.settings.autocomplete.source = $.proxy(this, '_source'); // use source method from class
       // add autocomplete functionality to text field
       this.$text.autocomplete(this.settings.autocomplete);
 
       // Our items have HTML tags.  The default rendering uses text()
       // to set the content of the <a> tag.  We need html().
-      this.$text.data("ui-autocomplete")._renderItem = function(ul, item) {
-        var label = $("<a>").html(item.label);
+      this.$text.data('ui-autocomplete')._renderItem = function(ul, item) {
+        var label = $('<a>').html(item.label);
         if (item.value === -1) {
-          label.addClass("ui-menu-noresults");
+          label.addClass('ui-menu-noresults');
         }
-        return $("<li>").append(label).appendTo(ul);
+        return $('<li>').append(label).appendTo(ul);
       };
 
       // set callbacks for autocomplete
@@ -93,15 +93,15 @@
 
     _source: function(request, response){
       var term = this.stripFormal(request.term),
-          matcher = new RegExp("^" + term, "i");
+          matcher = new RegExp('^' + term, 'i');
 
       if (this._hasMatches(request.term)) {
         response(
-          this.$select.children("option").map(function() {
+          this.$select.children('option').map(function() {
             var text = $( this ).text();
             if (this.value && (!request.term || matcher.test(text))){
               return {
-                label: request.term !== "" ? text.replace(new RegExp("^(?![^&;]+;)(?!<[^<>]*)(" + term + ")(?![^<>]*>)(?![^&;]+;)", "gi"), "<strong>$1</strong>") : text,
+                label: request.term !== '' ? text.replace(new RegExp('^(?![^&;]+;)(?!<[^<>]*)(' + term + ')(?![^<>]*>)(?![^&;]+;)', 'gi'), '<strong>$1</strong>') : text,
                 value: text,
                 option: this
               };
@@ -109,23 +109,23 @@
           })
         );
       } else {
-        response([{ label: "Prison not found", value: -1}]);
+        response([{ label: 'Prison not found', value: -1}]);
       }
     },
 
     stripFormal: function (term) {
       var matcher = term.match(/^(hmp?|yoi)? ?/i);
       if (matcher[0].length) {
-        term = term.replace(matcher[0], "");
+        term = term.replace(matcher[0], '');
       }
       return $.ui.autocomplete.escapeRegex(term);
     },
 
     _hasMatches: function (term) {
       var matched = false,
-          matcher = new RegExp("^" + this.stripFormal(term) + "[a-z]*", "i");
+          matcher = new RegExp('^' + this.stripFormal(term) + '[a-z]*', 'i');
       
-      this.$select.children("option").each(function() {
+      this.$select.children('option').each(function() {
         // if a match is found, select it and change to proper case in text field
         if (matcher.test($(this).text().toLowerCase())) {
           matched = true;
@@ -153,7 +153,7 @@
 
     _autocompletechange: function(event, ui) {
       var $text = $(this),
-          $select = $text.data("select"),
+          $select = $text.data('select'),
           value = $text.val(),
           valueLowerCase = value.toLowerCase(),
           valid = false;
@@ -164,7 +164,7 @@
       }
 
       // Search for a match (case-insensitive)
-      $select.children("option").each(function() {
+      $select.children('option').each(function() {
         // if a match is found, select it and change to proper case in text field
         if ($(this).text().toLowerCase() === valueLowerCase) {
           this.selected = valid = true;
@@ -179,19 +179,19 @@
       }
 
       // Remove invalid value
-      $text.val("").attr("title", value + " didn't match any item");
+      $text.val('').attr('title', value + ' didn\'t match any item');
       // clear value from select
-      $select.val("");
+      $select.val('');
       // remove value from autocomplete obj
-      $text.data("ui-autocomplete").term = "";
+      $text.data('ui-autocomplete').term = '';
     },
 
     // stop first item being selected when no value has been entered
     _autocompletesearch: function() {
-      if($(this).val() === "") {
-        $(this).data("ui-autocomplete").options.autoFocus = false;
+      if($(this).val() === '') {
+        $(this).data('ui-autocomplete').options.autoFocus = false;
       } else {
-        $(this).data("ui-autocomplete").options.autoFocus = true;
+        $(this).data('ui-autocomplete').options.autoFocus = true;
       }
     }
   };
@@ -208,13 +208,13 @@
 
 
 (function(){
-  "use strict";
+  'use strict';
 
   // Add module to MOJ namespace
   moj.Modules.autocomplete = {
     init: function () {
       // auto initate plugin if class is present
-      $(".js-autocomplete").mojAutocomplete($(this).data());
+      $('.js-autocomplete').mojAutocomplete($(this).data());
     }
   };
 }());
