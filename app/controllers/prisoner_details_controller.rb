931540a8ee63914f0f6d50bbc996649035d395f0
Jan Szumiec <jan.szumiec@gmail.com>
2015-02-06 11:07:27 +0000

    Added a counter for determining how often visitors get prisoner's number wrong.

--- app/controllers/prisoner_details_controller.rb
+++ app/controllers/prisoner_details_controller.rb
@@ -17,6 +17,9 @@ class PrisonerDetailsController < ApplicationController
         redirect_to deferred_edit_visitors_details_path
       end
     else
+      if visit.prisoner.errors[:number].any?
+        STATSD_CLIENT.increment('pvb.app.invalid_prisoner_number')
+      end
       redirect_to edit_prisoner_details_path
     end
   end
