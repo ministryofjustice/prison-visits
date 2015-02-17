a14b0fab89434bd847143b762e3e630900e97bbb
Jan Szumiec <jan.szumiec@gmail.com>
2015-02-17 13:43:16 +0000

    Removed no longer used views and actions.

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,6 @@ PrisonVisits2::Application.routes.draw do
   scope controller: :metrics do
     get "metrics", action: :index
     get "metrics/weekly", action: :weekly
-    get "metrics/other", action: :other, as: :other_metrics
     get "metrics/:prison/all_time", action: :all_time, as: :prison_metrics_all_time
     get "metrics/:prison/fortnightly_performance", action: :fortnightly_performance, as: :prison_metrics_fortnightly_performance
     get "metrics/:prison/fortnightly", action: :fortnightly, as: :prison_metrics_fortnightly
