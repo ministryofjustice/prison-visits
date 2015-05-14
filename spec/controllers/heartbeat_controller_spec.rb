5c4e72a8c0daa4d61ac8c72c397b474540b2c6bb
Steve Marshall <steve@nascentguruism.com>
2015-05-14 17:27:02 +0100

    Give all healthcheck specs a single purpose
    Each test now tests one thing and one thing only, and tests that are intentionally similar are split out into a `shared_examples` block so it's clearer that they're supposed to all behave in the same way (and we can keep them that way).

--- spec/controllers/heartbeat_controller_spec.rb
+++ spec/controllers/heartbeat_controller_spec.rb
@@ -27,47 +27,62 @@ describe HeartbeatController do
         controller.stub(:reject_without_key!)
       end
 
-      it "talks to sendgrid and messagelabs" do
-        set_mock_mailer_response(PrisonMailer, true)
-        set_mock_mailer_response(VisitorMailer, true)
-        get :healthcheck
+      context "when everything is OK" do
+        before :each do
+          set_mock_mailer_response(PrisonMailer, true)
+          set_mock_mailer_response(VisitorMailer, true)
+        end
 
-        assert_response(:success, response.status)
-        parsed_body = JSON.parse(response.body)
-        parsed_body['checks']['messagelabs'].should be_true
-        parsed_body['checks']['sendgrid'].should be_true
-        parsed_body['checks']['database'].should be_true
+        it "returns a HTTP Success status" do
+          get :healthcheck
+          assert_response(:success, response.status)
+        end
+
+        it "reports all services as OK" do
+          get :healthcheck
+          parsed_body = JSON.parse(response.body)
+          parsed_body['checks'].values.all?.should be_true
+        end
+
+        [
+          'sendgrid',
+          'messagelabs',
+          'database',
+        ].each do |service|
+          it "contains a check for #{service}" do
+            get :healthcheck
+            parsed_body = JSON.parse(response.body)
+            parsed_body['checks'].has_key?(service).should be_true
+          end
+        end
       end
 
-      it "errors if messagelabs is down" do
-        set_mock_mailer_response(PrisonMailer, false)
-        set_mock_mailer_response(VisitorMailer, true)
-        get :healthcheck
+      context "when messagelabs is down" do
+        before :each do
+          set_mock_mailer_response(PrisonMailer, false)
+          set_mock_mailer_response(VisitorMailer, true)
+        end
 
-        assert_response(:bad_gateway, response.status)
-        parsed_body = JSON.parse(response.body)
-        parsed_body['checks']['messagelabs'].should be_false
+        it_behaves_like "a service is broken", "messagelabs"
       end
 
-      it "errors if sendgrid is down" do
-        set_mock_mailer_response(PrisonMailer, true)
-        set_mock_mailer_response(VisitorMailer, false)
-        get :healthcheck
+      context "when sendgrid is down" do
+        before :each do
+          set_mock_mailer_response(PrisonMailer, true)
+          set_mock_mailer_response(VisitorMailer, false)
+        end
 
-        assert_response(:bad_gateway, response.status)
-        parsed_body = JSON.parse(response.body)
-        parsed_body['checks']['sendgrid'].should be_false
+        it_behaves_like "a service is broken", "sendgrid"
       end
 
-      it "errors if the database is down" do
-        set_mock_mailer_response(PrisonMailer, true)
-        set_mock_mailer_response(VisitorMailer, true)
-        ActiveRecord::Base.connection.should_receive(:active?).once.and_return(false)
-        get :healthcheck
+      context "when the database is down" do
+        before :each do
+          set_mock_mailer_response(PrisonMailer, true)
+          set_mock_mailer_response(VisitorMailer, true)
+          ActiveRecord::Base.connection.should_receive(:active?).once.and_return(false)
+        end
 
-        assert_response(:bad_gateway, response.status)
-        parsed_body = JSON.parse(response.body)
-        parsed_body['checks']['database'].should be_false
+        it_behaves_like "a service is broken", "database"
       end
     end
   end
