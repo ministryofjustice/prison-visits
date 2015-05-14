bb8b6038e0a60534111e7a63f076ec04a43dfe75
Steve Marshall <steve@nascentguruism.com>
2015-05-14 14:19:12 +0100

    Ensure healthcheck returns success as expected
    We should make sure the healthcheck behaves as we want in the default case. Also tweak the names of contexts to make the output easier to read.

--- spec/controllers/heartbeat_controller_spec.rb
+++ spec/controllers/heartbeat_controller_spec.rb
@@ -2,27 +2,33 @@ require 'spec_helper'
 
 describe HeartbeatController do
   render_views
-  
-  context "key restrictions" do
-    context "are enabled" do
+
+  context "with key restrictions" do
+    context "enabled" do
       it "raises an error" do
         Rails.configuration.metrics_auth_key = ""
         controller.should_receive(:reject!)
         get :healthcheck
       end
     end
-        
-    context "are disabled" do
+
+    context "disabled" do
       before :each do
         controller.stub(:reject_without_key!)
       end
 
       it "talks to sendgrid and messagelabs" do
         PrisonMailer.should_receive(:smtp_settings).and_return(address: host = double, port: port = double)
-        SendgridHelper.should_receive(:smtp_alive?).with(host, port).once
+        SendgridHelper.should_receive(:smtp_alive?).with(host, port).once.and_return(true)
         VisitorMailer.should_receive(:smtp_settings).and_return(address: host = double, port: port = double)
-        SendgridHelper.should_receive(:smtp_alive?).with(host, port).once
+        SendgridHelper.should_receive(:smtp_alive?).with(host, port).once.and_return(true)
         get :healthcheck
+
+        assert_response(:success, response.status)
+        parsed_body = JSON.parse(response.body)
+        parsed_body['checks']['messagelabs'].should be_true
+        parsed_body['checks']['sendgrid'].should be_true
+        parsed_body['checks']['database'].should be_true
       end
     end
   end
