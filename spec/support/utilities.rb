9ff2f2c79de010b11c3f7c710f0c806a87a24410
Dan Palmer <boss@bitrecursive.com>
2015-07-21 18:00:16 +0100

    Adds a Utilities module for specs
    Allows for dates to be reused across specs.
    
    Allows for prison instances to be constructed from dummy
    prison hashes.

---
+++ spec/support/utilities.rb
@@ -0,0 +1,25 @@
+require 'active_support'
+
+module Utilities
+  extend ActiveSupport::Concern
+
+  DAYS = {
+    monday:     Date.parse('Mon 13 July 2015'),
+    tuesday:    Date.parse('Tue 14 July 2015'),
+    wednesday:  Date.parse('Wed 15 July 2015'),
+    thursday:   Date.parse('Thu 16 July 2015'),
+    friday:     Date.parse('Fri 17 July 2015'),
+    saturday:   Date.parse('Sat 18 July 2015'),
+    sunday:     Date.parse('Sun 19 July 2015')
+  }.freeze
+
+  WEEKDAYS = DAYS.except(:saturday, :sunday).freeze
+
+  WEEKEND_DAYS = DAYS.slice(:saturday, :sunday).freeze
+
+  included do
+    def prison_from(prison_hash)
+      Prison.new 'Example Prison', prison_hash
+    end
+  end
+end
