f706ba947df2fafa72ec3602af66427c2acfdc85
Dan Palmer <boss@bitrecursive.com>
2015-07-24 16:06:30 +0100

    Refactor `prison_information_page` spec
    Tidy it up a little so it reads better.

--- spec/features/prison_information_page_spec.rb
+++ spec/features/prison_information_page_spec.rb
@@ -1,42 +1,38 @@
 require 'browserstack_helper'
 
-RSpec.feature "visitor enters prisoner information" do
-  include_examples "feature helper"
+RSpec.feature 'visitor entering prisoner information' do
+  include_examples 'feature helper'
 
-  [:deferred, :instant].each do |flow|
-    context "#{flow} flow" do
+  before(:each) { visit '/prisoner-details' }
 
-
-      context "and leaves fields blank" do
-        it "validation messages are present when a prison is not selected" do
-          visit '/prisoner-details'
-
-          click_button 'Continue'
-
-          expect(page).to have_css("label[for='prisoner_prison_name'] .validation-message")
+  %i<deferred instant>.each do |flow|
+    describe "#{flow} flow" do
+      describe 'page validations' do
+        context 'when a prison is not selected' do
+          it 'displays an error message' do
+            click_button 'Continue'
+            expect(page).to have_css("label[for='prisoner_prison_name'] .validation-message")
+          end
         end
 
-        it "validation messages are present" do
-          visit '/prisoner-details'
+        context 'when prisoner related form fields are left blank' do
+          let(:prisoner_element_ids) { %w<first_name last_name date_of_birth_3i number> }
 
-          find(:css, ".ui-autocomplete-input").set('Cardiff')
-          click_button 'Continue'
-
-          expect(page).to have_css(".validation-error #prisoner_first_name")
-          expect(page).to have_css(".validation-error #prisoner_last_name")
-          expect(page).to have_css(".validation-error #prisoner_date_of_birth_3i")
-          expect(page).to have_css(".validation-error #prisoner_number")
+          it 'displays error messages for each field' do
+            set_prison_to 'Cardiff'
+            click_button 'Continue'
+            prisoner_element_ids.each do |id|
+              expect(page).to have_css ".validation-error #prisoner_#{id}"
+            end
+          end
         end
       end
 
-      context "and they fill out all fields" do
-        it "prompts for visitor information" do
-          visit '/prisoner-details'
+      scenario 'user is taken to the vistor page when information is entered correctly' do
+        enter_prisoner_information(flow)
 
-          enter_prisoner_information(flow)
-
-          expect(page).to have_content('Visitor 1')
-        end
+        expect(page).to have_content('Visitor 1')
+        expect(page.current_path).to eq "/#{flow}/visitors"
       end
     end
   end
