3482754a3cb00ef23e4cac60f4d8486144e22374
Dan Palmer <boss@bitrecursive.com>
2015-07-24 16:39:54 +0100

    Improve `/prisoner` page feature spec
    Adds a number of tests for asserting error messages are displayed
    correctly depending on the prisons state.
    
    - when a prison is disabled
    - when a prison is coming soon
    - when a prison has IT issues

--- spec/features/prison_information_page_spec.rb
+++ spec/features/prison_information_page_spec.rb
@@ -11,7 +11,8 @@ RSpec.feature 'visitor entering prisoner information' do
         context 'when a prison is not selected' do
           it 'displays an error message' do
             click_button 'Continue'
-            expect(page).to have_css("label[for='prisoner_prison_name'] .validation-message")
+
+            expect(page).to have_css "label[for='prisoner_prison_name'] .validation-message"
           end
         end
 
@@ -21,14 +22,49 @@ RSpec.feature 'visitor entering prisoner information' do
           it 'displays error messages for each field' do
             set_prison_to 'Cardiff'
             click_button 'Continue'
+
             prisoner_element_ids.each do |id|
               expect(page).to have_css ".validation-error #prisoner_#{id}"
             end
           end
         end
+
+        context 'when a prison that is disabled is selected' do
+          let(:a_disabled_prison) { 'Rye Hill' }
+
+          it 'displays a message about the prison being disabled' do
+            set_prison_to a_disabled_prison
+            click_button 'Continue'
+
+            expect(page).to have_content 'HMP Rye Hill is unable to process online visit requests.'
+          end
+        end
+
+        context 'when a prison is coming soon' do
+          let(:a_prison_coming_soon) { 'Hull' }
+
+          it 'displays a message about the prison not being available just yet' do
+            set_prison_to a_prison_coming_soon
+            click_button 'Continue'
+
+            expect(page).to have_content 'HMP Hull isn’t able to process online visit requests yet.'
+          end
+        end
+
+        context 'when a prison has IT issues' do
+          let(:a_prison_with_it_issues) { 'Blantyre House' }
+
+          it 'displays a message about the prison not being available just yet' do
+            set_prison_to a_prison_with_it_issues
+            click_button 'Continue'
+
+            expect(page).
+              to have_content 'HMP Blantyre House is unable to process online visit requests right now.'
+          end
+        end
       end
 
-      scenario 'user is taken to the vistor page when information is entered correctly' do
+      scenario 'a user is taken to the vistor page when information is entered correctly' do
         enter_prisoner_information(flow)
 
         expect(page).to have_content('Visitor 1')
