844dbd725ae17558a8966476d8e7710f2aca1857
Paul Battley <pbattley@gmail.com>
2015-08-19 18:25:06 +0100

    Rewrite specs to express intention better
    One assertion (broadly) per spec, and the specific behaviours
    identified.

--- spec/controllers/feedbacks_controller_spec.rb
+++ spec/controllers/feedbacks_controller_spec.rb
@@ -1,52 +1,119 @@
 require 'rails_helper'
 
 RSpec.describe FeedbacksController, type: :controller do
-  before :each do
+  before do
     ActionMailer::Base.deliveries.clear
     allow_any_instance_of(EmailValidator).to receive(:validate)
+    allow(ZendeskHelper).to receive(:send_to_zendesk)
   end
 
-  it "displays feedback form" do
-    get :new
-    expect(response).to render_template('feedbacks/new')
-    expect(response).to be_success
-  end
-
-  it "redirects to show page on successful feedback submission" do
-    expect(FeedbackMailer).to receive(:new_feedback).once.and_call_original
-    expect(ZendeskHelper).to receive(:send_to_zendesk).once
-    expect {
-      post :create, feedback: { email: 'test@maildrop.dsd.io', text: 'feedback', referrer: 'ref' }
-      expect(response).to redirect_to feedback_path
-    }.to change { ActionMailer::Base.deliveries.size }.by(1)
-  end
-
-  it "redirects to show page on successful feedback submission when no email address is entered" do
-    expect(FeedbackMailer).to receive(:new_feedback).once.and_call_original
-    expect(ZendeskHelper).to receive(:send_to_zendesk).never
-    expect {
-      post :create, feedback: { email: '', text: 'feedback', referrer: 'ref' }
-      expect(response).to redirect_to feedback_path
-    }.to change { ActionMailer::Base.deliveries.size }.by(1)
-  end
-
-  it "displays the form again if a submission was unsuccessful" do
-    post :create, feedback: { email: 'test@maildrop.dsd.io' }
-    expect(response).to render_template('feedbacks/new')
-    expect(response).to be_success
-  end
-
-  context "there is an active booking session present" do
-    before :each do
-      session[:visit] = Visit.new(prisoner: Prisoner.new(prison_name: 'Rochester'))
+  context 'new' do
+    it 'responds with success' do
+      get :new
+      expect(response).to be_success
     end
 
-    it "extracts the prison name from the session" do
-      expect(ZendeskHelper).to receive(:send_to_zendesk).once { |feedback|
-        expect(feedback.prison).to eq('Rochester')
+    it 'displays feedback form' do
+      get :new
+      expect(response).to render_template('feedbacks/new')
+    end
+  end
+
+  context 'create' do
+    context 'with a successful feedback submission' do
+      let(:feedback_params) {
+        { email: 'test@maildrop.dsd.io', text: 'feedback', referrer: 'ref' }
       }
-      post :create, feedback: { email: 'test@maildrop.dsd.io', text: 'feedback', referrer: 'ref', prison: 'Rochester' }
-      expect(response).to redirect_to feedback_path
+
+      it 'redirects to show page' do
+        post :create, feedback: feedback_params
+        expect(response).to redirect_to feedback_path
+      end
+
+      it 'sends to ZenDesk' do
+        expect(ZendeskHelper).to receive(:send_to_zendesk).once do |feedback|
+          expect(feedback.email).to eq('test@maildrop.dsd.io')
+          expect(feedback.text).to eq('feedback')
+        end
+        post :create, feedback: feedback_params
+      end
+
+      it 'sends an email' do
+        expect {
+          post :create, feedback: feedback_params
+        }.to change { ActionMailer::Base.deliveries.size }.by(1)
+      end
+    end
+
+    context 'with no email address entered' do
+      let(:feedback_params) {
+        { email: '', text: 'feedback', referrer: 'ref' }
+      }
+
+      it 'redirects to feedback page' do
+        post :create, feedback: feedback_params
+        expect(response).to redirect_to feedback_path
+      end
+
+      it 'does not send to ZenDesk' do
+        expect(ZendeskHelper).to receive(:send_to_zendesk).never
+        post :create, feedback: feedback_params
+      end
+
+      it 'sends an email' do
+        expect {
+          post :create, feedback: feedback_params
+        }.to change { ActionMailer::Base.deliveries.size }.by(1)
+      end
+    end
+
+    context 'with no text entered' do
+      let(:feedback_params) {
+        { email: 'test@maildrop.dsd.io', text: '', referrer: 'ref' }
+      }
+
+      it 'responds with success' do
+        post :create, feedback: feedback_params
+        expect(response).to be_success
+      end
+
+      it 'does not send to ZenDesk' do
+        expect(ZendeskHelper).to receive(:send_to_zendesk).never
+        post :create, feedback: feedback_params
+      end
+
+      it 'does not send an email' do
+        expect {
+          post :create, feedback: feedback_params
+        }.not_to change { ActionMailer::Base.deliveries.size }
+      end
+
+      it 're-renders the feedback template' do
+        post :create, feedback: feedback_params
+        expect(response).to render_template('feedbacks/new')
+      end
+    end
+
+    context 'when an active booking session is present' do
+      let(:feedback_params) {
+        {
+          email: 'test@maildrop.dsd.io',
+          text: 'feedback',
+          referrer: 'ref'
+        }
+      }
+
+      before do
+        session[:visit] =
+          Visit.new(prisoner: Prisoner.new(prison_name: 'Rochester'))
+      end
+
+      it 'extracts the prison name from the session' do
+        expect(ZendeskHelper).to receive(:send_to_zendesk) do |feedback|
+          expect(feedback.prison).to eq('Rochester')
+        end
+        post :create, feedback: feedback_params
+      end
     end
   end
 end
