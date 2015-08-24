5b17d4c0f3a10b0c8faea6c3320dbc3cb30877ce
Paul Battley <pbattley@gmail.com>
2015-08-24 11:01:25 +0100

    Tidy up long lines
    The spec formatting was so pathologically perverse that it slowed Vim to
    a crawl attempting to move around it. And it was ugly.

--- spec/lib/zendesk_helper_spec.rb
+++ spec/lib/zendesk_helper_spec.rb
@@ -7,7 +7,12 @@ RSpec.describe ZendeskHelper do
   end
 
   let :feedback do
-    Feedback.new(text: 'text', email: 'email', referrer: 'ref', user_agent: 'Mozilla')
+    Feedback.new(
+      text: 'text',
+      email: 'email',
+      referrer: 'ref',
+      user_agent: 'Mozilla'
+    )
   end
 
   let :feedback_with_prison do
@@ -23,24 +28,39 @@ RSpec.describe ZendeskHelper do
   end
 
   it "sends a piece of feedback to zendesk" do
-    expect(ZendeskAPI::Ticket).to receive(:new).with(client, description: 'text', requester: { email: 'email', name: 'Unknown' }, custom_fields: [
-                                                                                                                             {id: '23730083', value: 'ref'},
-                                                                                                                             {id: '23757677', value: 'prison_visits'},
-                                                                                                                             {id: '23791776', value: 'Mozilla'},
-                                                                                                                             {id: '23984153', value: nil}
-                                                                                                                            ]).and_call_original
+    expect(ZendeskAPI::Ticket).
+      to receive(:new).
+      with(
+        client,
+        description: 'text',
+        requester: { email: 'email',
+        name: 'Unknown' },
+        custom_fields: [
+          {id: '23730083', value: 'ref'},
+          {id: '23757677', value: 'prison_visits'},
+          {id: '23791776', value: 'Mozilla'},
+          {id: '23984153', value: nil}
+        ]
+      ).and_call_original
     expect_any_instance_of(ZendeskAPI::Ticket).to receive(:save!).once
     subject.send_to_zendesk(feedback, client)
   end
 
   context "when a prison name is passed in" do
     it "sends a piece of feedback to zendesk with the prison name" do
-      expect(ZendeskAPI::Ticket).to receive(:new).with(client, description: 'text', requester: { email: 'email', name: 'Unknown' }, custom_fields: [
-                                                                                                                                                {id: '23730083', value: 'ref'},
-                                                                                                                                                {id: '23757677', value: 'prison_visits'},
-                                                                                                                                                {id: '23791776', value: 'Mozilla'},
-                                                                                                                                                {id: '23984153', value: 'Rochester'}
-                                                                                                                                               ]).and_call_original
+      expect(ZendeskAPI::Ticket).
+        to receive(:new).
+        with(
+          client,
+          description: 'text',
+          requester: { email: 'email', name: 'Unknown' },
+          custom_fields: [
+            {id: '23730083', value: 'ref'},
+            {id: '23757677', value: 'prison_visits'},
+            {id: '23791776', value: 'Mozilla'},
+            {id: '23984153', value: 'Rochester'}
+          ]
+        ).and_call_original
       expect_any_instance_of(ZendeskAPI::Ticket).to receive(:save!).once
       subject.send_to_zendesk(feedback_with_prison, client)
     end
