6596d34eb651df54e1a1ee82b6bcbf4a107e213c
Paul Battley <pbattley@gmail.com>
2015-08-25 11:10:21 +0100

    Refactor healthcheck controller specs

--- spec/controllers/healthcheck_controller_spec.rb
+++ spec/controllers/healthcheck_controller_spec.rb
@@ -3,97 +3,121 @@ require 'rails_helper'
 RSpec.describe HealthcheckController, type: :controller do
   render_views
 
-  context "with key restrictions" do
-    context "enabled" do
-      it "raises an error" do
-        Rails.configuration.metrics_auth_key = ""
-        expect(controller).to receive(:reject!)
-        get :index
-      end
+  let(:key) { 'super secret' }
+
+  around do |example|
+    original = Rails.configuration.metrics_auth_key
+    Rails.configuration.metrics_auth_key = key
+    example.call
+    Rails.configuration.metrics_auth_key = original
+  end
+
+  let(:parsed_body) {
+    JSON.parse(response.body)
+  }
+
+  let(:message_labs_address) { double }
+  let(:message_labs_port) { double }
+  let(:sendgrid_address) { double }
+  let(:sendgrid_port) { double }
+
+  before do
+    allow(SendgridHelper).to receive(:smtp_alive?).and_return(true)
+  end
+
+  shared_examples 'a service is broken' do |service|
+    before do
+      get :index, key: key
     end
 
-    context "disabled" do
-      def set_mock_mailer_response(mailer, status)
-        expect(mailer).to receive(:smtp_settings).and_return(
-          address: host = double,
-          port: port = double
-        )
-        expect(SendgridHelper).to receive(:smtp_alive?).with(
-          host, port
-        ).once.and_return(status)
-      end
-
-      before :each do
-        allow(controller).to receive(:reject_without_key!)
-      end
-
-      context "when everything is OK" do
-        before :each do
-          set_mock_mailer_response(PrisonMailer, true)
-          set_mock_mailer_response(VisitorMailer, true)
-        end
-
-        it "returns a HTTP Success status" do
-          get :index
-          assert_response(:success, response.status)
-        end
-
-        it "reports all services as OK" do
-          get :index
-          parsed_body = JSON.parse(response.body)
-          expect(parsed_body['checks'].values.all?).to be_truthy
-        end
-
-        [
-          'sendgrid',
-          'messagelabs',
-          'database',
-        ].each do |service|
-          it "contains a check for #{service}" do
-            get :index
-            parsed_body = JSON.parse(response.body)
-            expect(parsed_body['checks'].has_key?(service)).to be_truthy
-          end
-        end
-      end
-
-      context "when messagelabs is down" do
-        before :each do
-          set_mock_mailer_response(PrisonMailer, false)
-          set_mock_mailer_response(VisitorMailer, true)
-        end
-
-        it_behaves_like "a service is broken", "messagelabs"
-      end
-
-      context "when sendgrid is down" do
-        before :each do
-          set_mock_mailer_response(PrisonMailer, true)
-          set_mock_mailer_response(VisitorMailer, false)
-        end
-
-        it_behaves_like "a service is broken", "sendgrid"
-      end
-
-      context "when the database is down" do
-        before :each do
-          set_mock_mailer_response(PrisonMailer, true)
-          set_mock_mailer_response(VisitorMailer, true)
-          expect(ActiveRecord::Base.connection).to receive(:active?).once.and_return(false)
-        end
-
-        it_behaves_like "a service is broken", "database"
-      end
-
-      context "when the database is off" do
-        before :each do
-          set_mock_mailer_response(PrisonMailer, true)
-          set_mock_mailer_response(VisitorMailer, true)
-          expect(ActiveRecord::Base.connection).to receive(:active?).once.and_raise(PG::ConnectionBad)
-        end
-
-        it_behaves_like "a service is broken", "database"
-      end
+    it 'returns an HTTP Bad Gateway status' do
+      expect(response).to have_http_status(:bad_gateway)
     end
+
+    it "reports #{service} as inaccessible" do
+      expect(parsed_body).to include('checks' => include(service => false))
+    end
+  end
+
+  context 'with an invalid key' do
+    it 'rejects unauthenticated access' do
+      expect {
+        get :index, key: 'garbage'
+      }.to raise_exception(ActionController::RoutingError)
+    end
+  end
+
+  context 'when everything is OK' do
+    before do
+      get :index, key: key
+    end
+
+    it 'returns an HTTP Success status' do
+      expect(response).to be_success
+    end
+
+    it 'reports sendgrid as OK' do
+      expect(parsed_body).to include('checks' => include('sendgrid' => true))
+    end
+
+    it 'reports messagelabs as OK' do
+      expect(parsed_body).to include('checks' => include('messagelabs' => true))
+    end
+
+    it 'reports database as OK' do
+      expect(parsed_body).to include('checks' => include('database' => true))
+    end
+  end
+
+  context 'when messagelabs is down' do
+    before do
+      address = double
+      port = double
+      allow(PrisonMailer).
+        to receive(:smtp_settings).
+        and_return(address: address, port: port)
+      allow(SendgridHelper).
+        to receive(:smtp_alive?).
+        with(address, port).
+        and_return(false)
+    end
+
+    it_behaves_like 'a service is broken', 'messagelabs'
+  end
+
+  context 'when sendgrid is down' do
+    before do
+      address = double
+      port = double
+      allow(VisitorMailer).
+        to receive(:smtp_settings).
+        and_return(address: address, port: port)
+      allow(SendgridHelper).
+        to receive(:smtp_alive?).
+        with(address, port).
+        and_return(false)
+    end
+
+    it_behaves_like 'a service is broken', 'sendgrid'
+  end
+
+  context 'when the database is down' do
+    before do
+      allow(ActiveRecord::Base.connection).
+        to receive(:active?).
+        and_return(false)
+    end
+
+    it_behaves_like 'a service is broken', 'database'
+  end
+
+  context 'when the database is off' do
+    before do
+      allow(ActiveRecord::Base.connection).
+        to receive(:active?).
+        and_raise(PG::ConnectionBad)
+    end
+
+    it_behaves_like 'a service is broken', 'database'
   end
 end
