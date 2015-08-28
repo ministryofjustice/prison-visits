10e03ef16b70274cc3c3d5d9ef22e67d22780db8
Paul Battley <pbattley@gmail.com>
2015-08-28 13:00:52 +0100

    Make session configuration look like normal code.
    It was distractingly weird and almost incomprehensible.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,9 @@
 SERVICE_DOMAIN = (url = ENV['SERVICE_URL']) ? URI.parse(url).host : nil
 
-{key: 'pvbs', expire_after: 20.minutes, httponly: true, max_age: 20.minutes, domain: SERVICE_DOMAIN}.tap do |configuration|
-  configuration[:secure] = Rails.env.production?
-  PrisonVisits2::Application.config.session_store :cookie_store, configuration
-end
+PrisonVisits2::Application.config.session_store :cookie_store,
+  key: 'pvbs',
+  expire_after: 20.minutes,
+  httponly: true,
+  max_age: 20.minutes,
+  domain: SERVICE_DOMAIN,
+  secure: Rails.env.production?
