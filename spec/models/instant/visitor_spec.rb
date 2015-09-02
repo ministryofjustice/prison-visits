d861647d38803e164adfc7a3b0c652cebc52ac21
Paul Battley <pbattley@gmail.com>
2015-09-02 16:47:10 +0100

    Rewrite visitor specs to be comprehensible
    Between the unecessary use of Object#tap and the multiple expectations,
    it was hard to see what was being tested.
    
    Improved testing of age calculation, which turns out to be broken.

--- spec/models/instant/visitor_spec.rb
+++ spec/models/instant/visitor_spec.rb
@@ -1,36 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Instant::Visitor do
-  let :visitor do
-    subject.tap do |v|
-      v.first_name = 'Otto'
-      v.last_name = 'Fibonacci'
-      v.email = 'test@maildrop.dsd.io'
-      v.date_of_birth = 30.years.ago
-    end
-  end
-
-  it 'generates an initial from the last name' do
-    expect(visitor.last_initial).to eq('F')
-  end
-
   it_behaves_like 'a visitor'
-
-  it "validates the first visitor as a lead visitor" do
-    subject.tap do |v|
-      v.index = 0
-
-      v.first_name = 'Jimmy'
-      expect(v).not_to be_valid
-
-      v.last_name = 'Harris'
-      expect(v).not_to be_valid
-
-      v.date_of_birth = Date.parse "1986-04-20"
-      expect(v).not_to be_valid
-
-      v.email = 'jimmy@maildrop.dsd.io'
-      expect(v).to be_valid
-    end
-  end
 end
--- spec/support/visitor_support.rb
+++ spec/support/visitor_support.rb
@@ -1,46 +1,139 @@
-RSpec.shared_examples "a visitor" do
-  it "validates names" do
-    visitor.tap do |v|
-      v.index = 0
-      v.date_of_birth = Date.parse "1986-04-20"
+RSpec.shared_examples 'a visitor' do
+  before do
+    subject.index = 0
+  end
 
-      v.first_name = "<Jeremy"
-      expect(v).not_to be_valid
+  describe 'validation' do
+    context 'first name' do
+      it 'is invalid if it contains punctuation' do
+        subject.first_name = '<Jeremy'
+        subject.validate
+        expect(subject.errors[:first_name]).not_to be_empty
+      end
 
-      v.first_name = "Jeremy>"
-      expect(v).not_to be_valid
+      it 'is valid if it contains an ASCII apostrophe' do
+        subject.first_name = "O'Dear"
+        subject.validate
+        expect(subject.errors[:first_name]).to be_empty
+      end
 
-      v.first_name = "Manfred"
-      expect(v).to be_valid
+      it 'is valid if it contains a unicode apostrophe' do
+        subject.first_name = 'O’Dear'
+        subject.validate
+        expect(subject.errors[:first_name]).to be_empty
+      end
+    end
+
+    context 'last name' do
+      it 'is invalid if it contains punctuation' do
+        subject.last_name = '<Odear'
+        subject.validate
+        expect(subject.errors[:last_name]).not_to be_empty
+      end
+
+      it 'is valid if it contains an ASCII apostrophe' do
+        subject.last_name = "O'Dear"
+        subject.validate
+        expect(subject.errors[:last_name]).to be_empty
+      end
+
+      it 'is valid if it contains a unicode apostrophe' do
+        subject.last_name = 'O’Dear'
+        subject.validate
+        expect(subject.errors[:last_name]).to be_empty
+      end
+    end
+
+    context 'email' do
+      context 'for the first visitor' do
+        before do
+          subject.index = 0
+        end
+
+        it 'is invalid without an email' do
+          subject.email = nil
+          subject.validate
+          expect(subject.errors[:email]).not_to be_empty
+        end
+
+        it 'is valid with an email' do
+          subject.email = 'user@test.example.com'
+          subject.validate
+          expect(subject.errors[:email]).to be_empty
+        end
+      end
+
+      context 'for an additional visitor' do
+        before do
+          subject.index = 1
+        end
+
+        it 'is valid without an email' do
+          subject.email = nil
+          subject.validate
+          expect(subject.errors[:email]).to be_empty
+        end
+
+        it 'is invalid with an email' do
+          subject.email = 'user@test.example.com'
+          subject.validate
+          expect(subject.errors[:email]).not_to be_empty
+        end
+      end
+    end
+
+    context 'date of birth' do
+      it 'is valid when in a reasonable range' do
+        subject.date_of_birth = Date.new(1970, 1, 1)
+        subject.validate
+        expect(subject.errors[:date_of_birth]).to be_empty
+      end
+
+      it 'is invalid when outside a reasonable range' do
+        subject.date_of_birth = Date.new(1770, 1, 1)
+        subject.validate
+        expect(subject.errors[:date_of_birth]).to eq(['must be a valid date'])
+      end
+
+      it 'is invalid when missing' do
+        subject.date_of_birth = nil
+        subject.validate
+        expect(subject.errors[:date_of_birth]).to eq(['must be a valid date'])
+      end
     end
   end
 
-  it "displays a full_name" do
-    expect(visitor.full_name).to eq('Otto Fibonacci')
+  it 'forms full name' do
+    subject.first_name = 'Otto'
+    subject.last_name = 'Fibonacci'
+    expect(subject.full_name).to eq('Otto Fibonacci')
   end
 
-  it "returns the age of the visitor" do
-    expect(visitor.age).to eq(30)
-    visitor.date_of_birth = nil
-    expect(visitor.age).to be_nil
+  it 'generates an initial from the last name' do
+    subject.last_name = 'Fibonacci'
+    expect(subject.last_initial).to eq('F')
   end
 
-  (1..5).each do |i|
-    it "validates every #{i}-th visitor as an additional visitor" do
-      subject.tap do |v|
-        v.index = i
+  describe 'age' do
+    before do
+      subject.date_of_birth = Date.new(1980, 6, 1)
+    end
 
-        v.first_name = 'Jimmy'
-        expect(v).not_to be_valid
+    it 'is calculated on the day before the birthday' do
+      Timecop.freeze(Date.new(2015, 5, 31)) do
+        expect(subject.age).to eq(34)
+      end
+    end
 
-        v.last_name = 'Harris'
-        expect(v).not_to be_valid
+    it 'is calculated on the day after the birthday' do
+      Timecop.freeze(Date.new(2015, 6, 2)) do
+        expect(subject.age).to eq(35)
+      end
+    end
 
-        v.date_of_birth = Date.parse "1986-04-20"
-        expect(v).to be_valid
-
-        v.email = 'anything'
-        expect(v).not_to be_valid
+    it 'is calculated on the birthday' do
+      Timecop.freeze(Date.new(2015, 6, 1)) do
+        expect(subject.age).to eq(35)
       end
     end
   end
