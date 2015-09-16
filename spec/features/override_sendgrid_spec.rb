d647fa03f16f315d9a2b0231fd22ddbb1ec507f1
Paul Battley <pbattley@gmail.com>
2015-09-16 10:59:21 +0100

    Add end-to-end bounce override spec

--- spec/features/override_sendgrid_spec.rb
+++ spec/features/override_sendgrid_spec.rb
@@ -29,4 +29,26 @@ RSpec.feature "overriding Sendgrid" do
 
     expect(page).to have_content('Your request is being processed')
   end
+
+  scenario 'overriding bounce' do
+    allow(SendgridHelper).to receive(:bounced?).and_return(true)
+
+    visit '/prisoner-details'
+    enter_prisoner_information
+    enter_visitor_information
+    click_button 'Continue'
+
+    expect(page).to have_text('returned in the past')
+    check 'Tick this box to confirm you’d like us to try sending messages to you again'
+    click_button 'Continue'
+
+    expect(page).to have_content('When do you want to visit?')
+    select_a_slot
+    click_button 'Continue'
+
+    expect(page).to have_content('Check your request')
+    click_button 'Send request'
+
+    expect(page).to have_content('Your request is being processed')
+  end
 end
