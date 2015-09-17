296ac3a235e667dc7102caa7787c93aa83d9f76a
Evangelos Giataganas <e.giataganas@gmail.com>
2015-09-17 16:07:33 +0100

    Fix def end alignment

--- lib/rag_status_report.rb
+++ lib/rag_status_report.rb
@@ -12,16 +12,18 @@ class RagStatusReport
 
   def query(year, fortnight, percentile)
     @rag_count ||= rag_count(VisitMetricsEntry.find_by_sql([%Q{
-WITH ranked_times AS (
-  SELECT nomis_id, end_to_end_time, cume_dist() OVER (PARTITION BY nomis_id ORDER BY end_to_end_time)
-  FROM visit_metrics_entries
-  WHERE EXTRACT(isoyear FROM processed_at) = ?
-  AND EXTRACT(week from processed_at) / 2 = ?
-)
-SELECT nomis_id, min(end_to_end_time) as end_to_end_time from ranked_times
-WHERE cume_dist >= ?
-GROUP BY nomis_id}, year, fortnight, percentile]))
-end
+      WITH ranked_times AS (
+        SELECT nomis_id, end_to_end_time, cume_dist()
+        OVER (PARTITION BY nomis_id ORDER BY end_to_end_time)
+        FROM visit_metrics_entries
+        WHERE EXTRACT(isoyear FROM processed_at) = ?
+        AND EXTRACT(week from processed_at) / 2 = ?
+      )
+      SELECT nomis_id, min(end_to_end_time) as end_to_end_time
+      FROM ranked_times
+      WHERE cume_dist >= ?
+      GROUP BY nomis_id}, year, fortnight, percentile]))
+  end
 
   def rag_count(records)
     records.inject(Hash.new(0)) do |h, record|
