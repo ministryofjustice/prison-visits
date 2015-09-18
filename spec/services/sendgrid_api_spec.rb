b5be6bf9c0a90536038fd104db2212f84dc44c22
Dan Palmer <boss@bitrecursive.com>
2015-09-18 15:47:39 +0100

    Use RSpec `describe` for specific method contexts

--- spec/services/sendgrid_api_spec.rb
+++ spec/services/sendgrid_api_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe SendgridApi do
       Rails.configuration.action_mailer.smtp_settings = smtp_settings
     end
 
-    context 'spam reports' do
+    describe '.spam_reported?' do
       let(:body) { '[]' }
 
       before do
@@ -82,7 +82,7 @@ RSpec.describe SendgridApi do
       end
     end
 
-    context 'bounced' do
+    describe '.bounced?' do
       let(:body) { '[]' }
 
       before do
@@ -290,7 +290,7 @@ RSpec.describe SendgridApi do
       Rails.configuration.action_mailer.smtp_settings = smtp_settings
     end
 
-    context 'bounced?' do
+    describe '.bounced?' do
       it 'never says that the email has bounced' do
         expect(subject.bounced?('test@example.com')).to be_falsey
       end
@@ -301,7 +301,7 @@ RSpec.describe SendgridApi do
       end
     end
 
-    context 'spam_reported?' do
+    describe '.spam_reported?' do
       it 'never says that the email address has been reported for spam' do
         expect(subject.spam_reported?('test@example.com')).to be_falsey
       end
