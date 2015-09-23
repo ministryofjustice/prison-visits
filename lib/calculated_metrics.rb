3d1a8a83f27a777db97c65b7220ed72e7949b734
Evangelos Giataganas <e.giataganas@gmail.com>
2015-09-23 18:20:51 +0100

    Fix 'calculated_metrics' Line Length
    Fix 'lib/calculated_metrics.rb' so it will have
    80 characters max line length

--- lib/calculated_metrics.rb
+++ lib/calculated_metrics.rb
@@ -1,7 +1,9 @@
 require 'csv'
 
 class CalculatedMetrics
-  attr_reader :total_visits, :waiting_visits, :overdue_visits, :confirmed_visits, :rejected_visits, :rejected_for_reason, :end_to_end_median_times, :end_to_end_times, :processing_times
+  attr_reader :total_visits, :waiting_visits, :overdue_visits,
+    :confirmed_visits, :rejected_visits, :rejected_for_reason,
+    :end_to_end_median_times, :end_to_end_times, :processing_times
 
   def initialize(model, overdue_threshold, date_range=nil)
     @model = model
@@ -13,7 +15,9 @@ class CalculatedMetrics
 
   def scope
     if @date_range
-      @model.where('requested_at IS NULL OR requested_at > ?', @date_range.first).where('processed_at IS NULL OR processed_at <= ?', @date_range.last)
+      @model.
+        where('requested_at IS NULL OR requested_at > ?', @date_range.first).
+        where('processed_at IS NULL OR processed_at <= ?', @date_range.last)
     else
       @model
     end
@@ -22,15 +26,23 @@ class CalculatedMetrics
   def refresh
     @total_visits = scope.group(:nomis_id).count
     @waiting_visits = scope.group(:nomis_id).where(processed_at: nil).count
-    @overdue_visits = scope.group(:nomis_id).where("processed_at IS NULL AND ? - requested_at > INTERVAL '? seconds'", Time.now, @overdue_threshold).count
-    @confirmed_visits = scope.group(:nomis_id).where(outcome: 'confirmed').count
+    @overdue_visits = scope.group(:nomis_id).
+      where(
+        "processed_at IS NULL AND ? - requested_at > INTERVAL '? seconds'",
+        Time.now, @overdue_threshold).count
+    @confirmed_visits = scope.group(:nomis_id).where(outcome: 'confirmed').
+      count
     @rejected_visits = scope.group(:nomis_id).where(outcome: 'rejected').count
-    @rejected_for_reason = scope.group(:nomis_id, :reason).where(outcome: 'rejected').count
+    @rejected_for_reason = scope.group(:nomis_id, :reason).
+      where(outcome: 'rejected').count
     @end_to_end_median_times = calculate_percentiles('end_to_end_time', 0.5)
     @end_to_end_times = calculate_percentiles('end_to_end_time', 0.95)
     @processing_times = calculate_percentiles('processing_time', 0.95)
 
-    [@total_visits, @waiting_visits, @overdue_visits, @confirmed_visits, @rejected_visits, @rejected_for_reason].each do |hash|
+    [
+      @total_visits, @waiting_visits, @overdue_visits, @confirmed_visits,
+      @rejected_visits, @rejected_for_reason
+    ].each do |hash|
       hash.default = 0
     end
 
@@ -39,7 +51,8 @@ class CalculatedMetrics
 
   def percent_rejected(prison, reason=nil)
     if reason
-      1.0 * (@rejected_for_reason[[prison, reason]] || 0) / @total_visits[prison]
+      1.0 * (@rejected_for_reason[[prison, reason]] || 0) /
+        @total_visits[prison]
     else
       1.0 * (@rejected_visits[prison] || 0) / @total_visits[prison]
     end
@@ -47,36 +60,55 @@ class CalculatedMetrics
 
   private
   def calculate_percentiles(column, percentile)
-    # This is a truly horrible hack. It lets us get past AR built-in value quoting, by pretending to be another object.
-    # It is needed so that we can parametrize two queries below by column, without resorting to putting the query together
-    # by hand. Don't call this method from outside this class, please.
+    # This is a truly horrible hack. It lets us get past AR built-in value
+    # quoting, by pretending to be another object. It is needed so that we
+    # can parametrize two queries below by column, without resorting to
+    # putting the query together by hand. Don't call this method from outside
+    # this class, please.
     column.define_singleton_method(:quoted_id) do
       column
     end
-    (@date_range ? calculate_percentiles_with_date_range(column, percentile) : calculate_percentiles_without_date_range(column, percentile)).each_with_object({}) do |row, h|
+    if @date_range
+      calculate_percentiles_with_date_range(column, percentile)
+    else
+      calculate_percentiles_without_date_range(column, percentile)
+    end.each_with_object({}) do |row, h|
       h[row['nomis_id']] = row[column].to_i
     end
   end
 
   def calculate_percentiles_with_date_range(column, percentile)
-    @model.find_by_sql ['
-WITH percentiles AS (SELECT nomis_id, ?, cume_dist() OVER (PARTITION BY nomis_id ORDER BY ?) AS percentile
-                     FROM visit_metrics_entries WHERE ? IS NOT NULL
-                     AND requested_at > ?::date
-                     AND processed_at <= ?::date),
-     top_percentiles AS (SELECT nomis_id, ?, rank() OVER (PARTITION BY nomis_id ORDER BY ?)
-                     FROM percentiles WHERE percentile >= ?)
-SELECT nomis_id, ? FROM top_percentiles WHERE rank = 1
-', column, column, column, @date_range.first, @date_range.last, column, column, percentile, column]
+    query = <<-SQL
+      WITH percentiles AS (SELECT nomis_id, ?, cume_dist()
+                            OVER (PARTITION BY nomis_id ORDER BY ?
+                          ) AS percentile
+                           FROM visit_metrics_entries WHERE ? IS NOT NULL
+                           AND requested_at > ?::date
+                           AND processed_at <= ?::date),
+           top_percentiles AS (SELECT nomis_id, ?, rank()
+                                OVER (PARTITION BY nomis_id ORDER BY ?)
+                           FROM percentiles WHERE percentile >= ?)
+      SELECT nomis_id, ? FROM top_percentiles WHERE rank = 1
+    SQL
+
+    @model.find_by_sql [query, column, column, column,
+                        @date_range.first,
+                        @date_range.last, column, column, percentile, column]
   end
 
   def calculate_percentiles_without_date_range(column, percentile)
-    @model.find_by_sql ['
-WITH percentiles AS (SELECT nomis_id, ?, cume_dist() OVER (PARTITION BY nomis_id ORDER BY ?) AS percentile
-                     FROM visit_metrics_entries WHERE ? IS NOT NULL),
-     top_percentiles AS (SELECT nomis_id, ?, rank() OVER (PARTITION BY nomis_id ORDER BY ?)
-                     FROM percentiles WHERE percentile >= ?)
-SELECT nomis_id, ? FROM top_percentiles WHERE rank = 1
-', column, column, column, column, column, percentile, column]
+    query = <<-SQL
+      WITH percentiles AS (SELECT nomis_id, ?, cume_dist()
+                            OVER (PARTITION BY nomis_id ORDER BY ?
+                          ) AS percentile
+                           FROM visit_metrics_entries WHERE ? IS NOT NULL),
+           top_percentiles AS (SELECT nomis_id, ?, rank()
+                                OVER (PARTITION BY nomis_id ORDER BY ?)
+                           FROM percentiles WHERE percentile >= ?)
+      SELECT nomis_id, ? FROM top_percentiles WHERE rank = 1
+    SQL
+
+    @model.find_by_sql [query, column, column, column, column,
+                        column, percentile, column]
   end
 end
