01bcf0bd1333128f3f1737eaa19b19ebe151fbaf
Evangelos Giataganas <e.giataganas@gmail.com>
2015-09-23 11:08:41 +0100

    Fix 'prisoner' Line Length
    Fix 'app/models/prisoner.rb' so it will have
    80 characters max line length

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -5,12 +5,19 @@ class Prisoner
   attribute :number, String
   attribute :prison_name, String
 
-  validates_format_of :number, with: /\A[a-z]\d{4}[a-z]{2}\z/i, message: "must be a valid prisoner number" # eg a1234aa
-  validates_inclusion_of :prison_name, in: Rails.configuration.prison_data.keys, message: "must be chosen"
+  validates :number, format: {
+    with: /\A[a-z]\d{4}[a-z]{2}\z/i
+  }
+
+  validates :prison_name, inclusion: {
+    in: Rails.configuration.prison_data.keys
+  }
+
   validate :prison_in_service
 
   def prison_in_service
-    if !self.prison_name.blank? && !Rails.configuration.prison_data[self.prison_name]['enabled']
+    if prison_name.present? &&
+      !Rails.configuration.prison_data[prison_name]['enabled']
       errors.add(:prison_name, 'is not available')
       errors.add(:prison_name_reason, true)
     end
