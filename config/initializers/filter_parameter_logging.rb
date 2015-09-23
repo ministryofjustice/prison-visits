54b48b10011428965a237707132df6990cb11249
Evangelos Giataganas <e.giataganas@gmail.com>
2015-09-23 15:12:12 +0100

    Fix 'filter_parameter_logging' Line Length
    Fix 'config/initializers/filter_parameter_logging.rb' so it will have
    80 characters max line length

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,1 +1,10 @@
-Rails.application.config.filter_parameters += [:password, :first_name, :last_name, :number, :'date_of_birth(3i)', :'date_of_birth(2i)', :'date_of_birth(1i)', :email]
+Rails.application.config.filter_parameters += [
+  :password,
+  :first_name,
+  :last_name,
+  :number,
+  :'date_of_birth(3i)',
+  :'date_of_birth(2i)',
+  :'date_of_birth(1i)',
+  :email
+]
