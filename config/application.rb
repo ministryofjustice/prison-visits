3149ddd87e589e9acb2297542ab9d3e13b9f7a17
Evangelos Giataganas <e.giataganas@gmail.com>
2015-09-23 14:41:36 +0100

    Fix 'application' Line Length
    Fix 'config/application.rb' so it will have
    80 characters max line length

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module PrisonVisits2
     config.phase = 'live'
     config.product_type = 'service'
     config.feedback_url = 'test@example.com'
-    config.unsubscribe_url = '<https://www.prisonvisits.service.gov.uk/unsubscribe>'
+    config.unsubscribe_url =
+      '<https://www.prisonvisits.service.gov.uk/unsubscribe>'
     config.ga_id = ENV['GA_TRACKING_ID']
     config.assets.enabled = true
     config.assets.precompile += %w(
@@ -24,7 +25,9 @@ module PrisonVisits2
       back-office.css
       *.png
     )
-    config.prison_data = YAML.load_file(ENV['PRISON_DATA_FILE'] || File.join(Rails.root, 'config', 'prison_data_staging.yml')).with_indifferent_access
+    config.prison_data = YAML.load_file(ENV['PRISON_DATA_FILE'] ||
+      File.join(Rails.root, 'config', 'prison_data_staging.yml')).
+        with_indifferent_access
     config.nomis_ids = config.prison_data.sort_by do |prison_name, _prison_data|
       prison_name
     end.inject(Set[]) do |set, (_prison_name, prison_data)|
@@ -39,9 +42,13 @@ module PrisonVisits2
             fetch('events').
             map { |event| Date.parse(event['date']) }
     config.metrics_auth_key = ENV['METRICS_AUTH_KEY']
-    config.permitted_ips_for_confirmations = (ENV['PRISON_ESTATE_IPS'] || '').split(',')
+    config.permitted_ips_for_confirmations =
+      (ENV['PRISON_ESTATE_IPS'] || '').split(',')
+
     config.autoload_paths += %w(lib app/mailers/concerns)
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'moj.slot-picker', 'dist', 'stylesheets')
+
+    config.assets.paths << Rails.root.join('vendor',
+      'assets', 'moj.slot-picker', 'dist', 'stylesheets')
     config.middleware.delete 'ActiveRecord::QueryCache'
     config.session_expire_after = 20.minutes
   end
