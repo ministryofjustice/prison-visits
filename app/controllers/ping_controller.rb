424b333c708fdaf8c9d521c1260035ffb252b670
Todd Tyree <tatyree@gmail.com>
2015-09-25 12:55:39 +0100

    Remove extra whitespace from ping_controller
    As per .rubocop_todo.yml.
    
    Also converted hashrocket hash syntax.

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,10 +1,10 @@
-class PingController  < ApplicationController
+class PingController < ApplicationController
   def index
     render json: {
-      :version_number => VERSION_INFO['version_number'],
-      :build_date => VERSION_INFO['build_date'],
-      :commit_id => VERSION_INFO['commit'],
-      :build_tag => VERSION_INFO['build_tag']
+      version_number:  VERSION_INFO['version_number'],
+      build_date:      VERSION_INFO['build_date'],
+      commit_id:       VERSION_INFO['commit'],
+      build_tag:       VERSION_INFO['build_tag']
     }
   end
 end
