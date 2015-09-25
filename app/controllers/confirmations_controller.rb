120290656ad2cb51df5ddec7dcd7bc269cec4f73
Evangelos Giataganas <e.giataganas@gmail.com>
2015-09-25 12:49:12 +0100

    Fix 'confirmation_controllers' Method Length
    Fix 'app/controllers/confirmations_controller.rb' so it will
    pass max method length

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -7,16 +7,8 @@ class ConfirmationsController < ApplicationController
     @confirmation = Confirmation.new
     metrics_logger.record_link_click(booked_visit)
 
-    if metrics_logger.processed?(booked_visit)
-      reset_session
-      STATSD_CLIENT.increment('pvb.app.already_booked')
-      render '_already_booked'
-    end
-
-    if metrics_logger.request_cancelled?(booked_visit)
-      reset_session
-      render '_request_cancelled'
-    end
+    metrics_logger_processed?(booked_visit)
+    metrics_logger_request_cancelled?(booked_visit)
 
     logstasher_add_visit_id(booked_visit.visit_id)
   end
@@ -27,37 +19,11 @@ class ConfirmationsController < ApplicationController
       return render :new
     end
 
-    if @confirmation.slot_selected?
-      token = encryptor.encrypt_and_sign(
-        attach_vo_number(
-          remove_unused_slots(
-            booked_visit, @confirmation.slot),
-          @confirmation)
-      )
-
-      VisitorMailer.booking_confirmation_email(
-        booked_visit,
-        @confirmation,
-        token
-      ).deliver_later
-
-      STATSD_CLIENT.increment("pvb.app.visit_confirmed")
-      metrics_logger.record_booking_confirmation(booked_visit)
-    else
-      VisitorMailer.booking_rejection_email(
-        booked_visit, @confirmation
-      ).deliver_later
-
-      STATSD_CLIENT.increment("pvb.app.visit_rejected")
-      metrics_logger.record_booking_rejection(
-        booked_visit,
-        @confirmation.outcome
-      )
-    end
+    confirmation_slot_selected?
     PrisonMailer.booking_receipt_email(booked_visit, @confirmation).
       deliver_later
 
-    STATSD_CLIENT.increment("pvb.app.visit_processed")
+    statsd_client_increment('visit_processed')
     redirect_to show_confirmation_path(visit_id: booked_visit.visit_id)
   end
 
@@ -96,5 +62,84 @@ class ConfirmationsController < ApplicationController
 
   def attach_vo_number(visit, confirmation)
     visit.dup.tap { |v| v.vo_number = confirmation.vo_number }
+  end
+
+  private
+
+  def metrics_logger_processed?(booked_visit)
+    if metrics_logger.processed?(booked_visit)
+      reset_session
+      STATSD_CLIENT.increment('pvb.app.already_booked')
+      render '_already_booked'
+    end
+  end
+
+  def metrics_logger_request_cancelled?(booked_visit)
+    if metrics_logger.request_cancelled?(booked_visit)
+      reset_session
+      render '_request_cancelled'
+    end
+  end
+
+  def confirmation_slot_selected?
+    if @confirmation.slot_selected?
+      booking_confirmation_email
+    else
+      booking_rejection_email
+    end
+  end
+
+  def booking_confirmation_email
+    token = encryptor_encrypt_and_sign
+
+    visitor_mailer_booking_confirmation_email(token)
+    statsd_client_increment('visit_confirmed')
+    metrics_logger_record_booking_confirmation
+  end
+
+  def booking_rejection_email
+    visitor_mailer_booking_rejection_email
+    statsd_client_increment('visit_rejected')
+    metrics_logger_record_booking_rejection
+  end
+
+  def encryptor_encrypt_and_sign
+    encryptor.encrypt_and_sign(
+      attach_vo_number(
+        remove_unused_slots(
+          booked_visit, @confirmation.slot),
+        @confirmation
+      )
+    )
+  end
+
+  def visitor_mailer_booking_confirmation_email(token)
+    VisitorMailer.booking_confirmation_email(
+      booked_visit,
+      @confirmation,
+      token
+    ).deliver_later
+  end
+
+  def visitor_mailer_booking_rejection_email
+    VisitorMailer.booking_rejection_email(
+      booked_visit,
+      @confirmation
+    ).deliver_later
+  end
+
+  def metrics_logger_record_booking_confirmation
+    metrics_logger.record_booking_confirmation(booked_visit)
+  end
+
+  def metrics_logger_record_booking_rejection
+    metrics_logger.record_booking_rejection(
+      booked_visit,
+      @confirmation.outcome
+    )
+  end
+
+  def statsd_client_increment(visit = '')
+    STATSD_CLIENT.increment("pvb.app.#{visit}")
   end
 end
