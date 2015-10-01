02763f576a31224d774d89852f16a068078395df
Evangelos Giataganas <e.giataganas@gmail.com>
2015-10-01 14:10:59 +0100

    Remove ':max_age', it duplicates expire_after
    We know that we don't need this because it has exactly the same function
    as `:expire_after`: Rails's `CookieStore` is a subclass of
    `Rack::Session::Abstract::ID` in which they are treated identically.
    
    https://github.com/rails/rails/blob/3ac3760c69e6e6914c5ddae138856b3c82ac0f20/actionpack/lib/action_dispatch/middleware/session/cookie_store.rb#L60
    https://github.com/rack/rack/blob/028438ffffd95ce1f6197d38c04fa5ea6a034a85/lib/rack/session/abstract/id.rb#L349-L350,

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,6 +5,5 @@ PrisonVisits2::Application.config.session_store :cookie_store,
   key: 'pvbs',
   expire_after: 20.minutes,
   httponly: true,
-  max_age: 20.minutes,
   domain: SERVICE_DOMAIN,
   secure: Rails.env.production?
--- spec/configuration/session_cookie_spec.rb
+++ spec/configuration/session_cookie_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe PrisonVisits2::Application.config do
   it "sets a cookie with the right parameters" do
     expect(subject.session_store).to eq(ActionDispatch::Session::CookieStore)
     # secure: false -> true gets set by the initializer.
-    expect(subject.session_options).to eq({key: "pvbs", expire_after: 20.minutes, httponly: true, cookie_only: true, secure: false, max_age: 1200, domain: nil})
+    expect(subject.session_options).to eq({key: "pvbs", expire_after: 20.minutes, httponly: true, cookie_only: true, secure: false, domain: nil})
   end
 end
