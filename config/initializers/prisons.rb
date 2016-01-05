a39d07a1ed6cdaa37ee1b6c8872a662cfa3df3e1
Paul Battley <pbattley@gmail.com>
2016-01-05 10:34:40 +0000

    Obviate the need for empty YAML defaults
    By providing sensible defaults, we no longer need to put lines like
    `unbookable: []` in the YAML.

--- config/initializers/prisons.rb
+++ config/initializers/prisons.rb
@@ -1,7 +1,13 @@
 Rails.configuration.prisons = []
 
+default = {
+  slot_anomalies: {},
+  slots: {},
+  unbookable: []
+}
+
 Dir["config/prisons/*.yml"].each { |file|
-  prison = YAML.load_file(file)
+  prison = default.merge(YAML.load_file(file))
   if ENV['GSI_SMTP_HOSTNAME'] == 'maildrop.dsd.io' || Rails.env.test?
     prison['email'] = "pvb.#{prison['nomis_id']}@maildrop.dsd.io"
   end
